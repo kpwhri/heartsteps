82c6ee3ac5a5d3248e3ce02ce106183fac5485d7
Nick Reid <nickreid@nickreid.com>
2018-06-06 15:55:18 -0700

    Added enroll endpoint that accepts post requests

--- server/heartsteps/urls.py
+++ server/heartsteps/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'', include('participants.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'api-auth', include('rest_framework.urls'))
 ]
--- server/participants/views.py
+++ server/participants/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework.authtoken.models import Token
+
+from .models import Participant
+
+@api_view(['POST'])
+def enroll(request):
+    if request.method == 'POST' and request.data.get('enrollment_token'):
+        try:
+            participant = Participant.objects.get()
+            token, created = Token.objects.get_or_create(user=participant.user)
+            return Response({
+                'token': token.key,
+                'participant_id': participant.id
+            })
+        except Participant.DoesNotExist:
+            pass
+    return Response({}, status=status.HTTP_400_BAD_REQUEST)
