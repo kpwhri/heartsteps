afef3a75b03ad668d08338620fbb47c29846feac
Nick Reid <nickreid@nickreid.com>
2018-06-06 15:54:44 -0700

    fixed typo in rest framework authentication

--- server/heartsteps/settings.py
+++ server/heartsteps/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
