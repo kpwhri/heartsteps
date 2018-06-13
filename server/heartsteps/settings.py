a7b922ea17b43ee3b1c261c3fdffa4dfebe6a79d
Nick Reid <nickreid@nickreid.com>
2018-06-13 15:55:52 -0700

    Added gae deployment for server

--- server/heartsteps/settings.py
+++ server/heartsteps/settings.py
@@ -2,8 +2,17 @@ import os, environ
 
 env = environ.Env()
 
+if(os.path.exists('/server/.env')):
+    env.read_env('/server/.env')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+root = environ.Path(BASE_DIR)
+SITE_ROOT = root()
+
+STATIC_ROOT = root('static')
+STATIC_URL = '/static/'
 
 SECRET_KEY = env('SECRET_KEY', default='secret-key')
 
