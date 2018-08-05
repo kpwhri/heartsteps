affb94cccbd07ffc1c966547452c7e595b805b43
Nick Reid <nickreid@nickreid.com>
2018-08-05 14:35:40 -0700

    Add error message to enrollment form

--- client/src/pages/enroll/enroll.ts
+++ client/src/pages/enroll/enroll.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnrollmentService } from '../../heartsteps/enrollment.service';
 import { loadingService } from '../../infrastructure/loading.service';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -10,28 +11,35 @@ import { loadingService } from '../../infrastructure/loading.service';
     providers: [ EnrollmentService ]
 })
 export class EnrollPage {
-    private enrollmentToken:String
-    private error:Boolean
+    public error:Boolean
+    public enrollmentForm:FormGroup
 
     constructor(
         private enrollmentService: EnrollmentService,
         private loadingService:loadingService
-    ) {}
+    ) {
+        this.enrollmentForm = new FormGroup({
+            entryToken: new FormControl('', Validators.required)
+        })
+    }
 
     enroll() {
-        if(!this.enrollmentToken || this.enrollmentToken==="") {
-            return
-        }
         this.error = false
 
-        this.loadingService.show('Authenticating entry code')
-        this.enrollmentService.enroll(this.enrollmentToken)
-        .then(() => {
-            this.loadingService.dismiss()  
-        })
-        .catch(() => {
-            this.error = true
-            this.loadingService.dismiss()
-        })
+        if(this.enrollmentForm.valid) {
+            this.loadingService.show('Authenticating entry code')
+    
+            let token = this.enrollmentForm.value.entryToken
+            this.enrollmentService.enroll(token)
+            .then(() => {
+                this.loadingService.dismiss()  
+            })
+            .catch(() => {
+                this.error = true
+                this.loadingService.dismiss()
+            })
+        } else {
+            this.enrollmentForm.markAsDirty()
+        }
     }
 }
