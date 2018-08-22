9c7c924f2542d9a27be318b281e118587b8fb379
Christopher Mack <christopher@mackdata.com>
2018-08-22 10:00:25 -0700

    Put fitbit URLs under /api for dispatch.yaml

--- server/heartsteps/urls.py
+++ server/heartsteps/urls.py
@@ -10,9 +10,7 @@ urlpatterns = [
     url(r'^api/', include('locations.urls')),
     url(r'^api/', include('participants.urls')),
     url(r'^api/', include('randomization.urls')),
+    url(r'^api/fitbit/', include('fitapp.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'api-auth', include('rest_framework.urls')),
-    url(r'^weather/', include('weather.urls')),
-    url(r'^fitbit/', include('fitapp.urls')),
-    url(r'^$', trackers.views.index, name='index')
 ]
