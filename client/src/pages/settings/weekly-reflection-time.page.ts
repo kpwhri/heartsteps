197fc428ffce8bc730bf07379e401ea767b9d66e
Nick Reid <nickreid@nickreid.com>
2018-09-04 15:12:01 -0700

    Organized forms css

--- client/src/pages/settings/weekly-reflection-time.page.ts
+++ client/src/pages/settings/weekly-reflection-time.page.ts
@@ -11,11 +11,44 @@ import { HeartstepsServer } from '@infrastructure/heartsteps-server.service';
 })
 export class WeeklyReflectionTimePage {
 
+    days:Array<string> = [
+        'monday',
+        'tuesday',
+        'wednesday',
+        'thursday',
+        'friday',
+        'saturday',
+        'sunday'
+    ]
+
+    weeklyReflectionForm: FormGroup;
+
     constructor(
         private navCtrl:NavController,
         private loadingService:loadingService,
         private heartstepsServer:HeartstepsServer
-    ) {}
+    ) {
+        this.weeklyReflectionForm = new FormGroup({
+            day: new FormControl('sunday', Validators.required),
+            time: new FormControl('20:00', Validators.required)
+        })
+    }
 
+    save() {
+        if(this.weeklyReflectionForm.valid) {
+            this.loadingService.show('Saving reflection time')
+            this.heartstepsServer.post('reflection-time', this.weeklyReflectionForm.value)
+            .then(() => {
+                this.navCtrl.pop()
+            })
+            .catch(() => {
+
+            })
+            .then(() => {
+                this.loadingService.dismiss()
+            })
+        }
+    }
+    
 
 }
