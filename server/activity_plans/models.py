4c41ddafb1d0981a88582dfaac49eb25bee6050a
Nick Reid <nickreid@nickreid.com>
2018-09-05 13:21:44 -0700

    Refactored activity plans to use abstract model for plan and log

--- server/activity_plans/models.py
+++ server/activity_plans/models.py
@@ -1,27 +1,41 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
-INTENSITY = [
-    ('m', 'Moderate'),
-    ('v', 'Vigorous')
-]
+class ActivityType(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
 
+    user = models.ForeignKey(User, null=True, blank=True)
 
-class ActivityPlan(models.Model):
+class AbstractActivity(models.Model):
+    class Meta:
+        abstract = True
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User)
-    activity_date = models.DateField()
-    start_time = models.CharField(max_length=15)
-    activity_type = models.CharField(max_length=50)
-    intensity = models.CharField(max_length=1, choices=INTENSITY)
-    duration = models.IntegerField()
-    complete = models.BooleanField(default=False)
-    enjoyed = models.IntegerField()
+
+    type = models.ForeignKey(ActivityType)
+    vigorous = models.BooleanField(default=False)
+    
+    start = models.DateTimeField()
+    duration = models.PositiveIntegerField()
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class ActivityLog(AbstractActivity):
+    enjoyed = models.IntegerField()
+
+    def __str__(self):
+        return "%s on %s (%s)" % (self.type, self.start, self.user)
+
+class ActivityPlan(AbstractActivity):
+    log = models.ForeignKey(ActivityLog, null=True, blank=True)
 
     def __str__(self):
         if self.complete:
             verb = "completed on"
         else:
             verb = "planned for"
-        return "%s %s %s (%s)" % (self.activity_type, verb, self.activity_date, self.user)
+        return "%s %s %s (%s)" % (self.type, verb, self.start, self.user)
