9da3bd64966efe7f60f3b4abb5f92b914d23f213
Nick Reid <nickreid@nickreid.com>
2018-09-09 20:14:29 -0700

    Added some styles

--- client/src/pages/enroll/enroll.ts
+++ client/src/pages/enroll/enroll.ts
@@ -39,7 +39,8 @@ export class EnrollPage {
                 this.loadingService.dismiss()
             })
         } else {
-            this.enrollmentForm.markAsDirty()
+            this.enrollmentForm.markAsDirty();
+            this.enrollmentForm.markAsTouched();
         }
     }
 }
