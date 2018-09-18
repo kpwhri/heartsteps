9910e4a0decf60b6010f4c820a2355d19a836c44
Christopher Mack <christopher@mackdata.com>
2018-09-18 16:58:27 -0700

    Same as above with corsheaders spelled correctly

--- server/heartsteps/settings.py
+++ server/heartsteps/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'contact',
-    'coresheaders',
+    'corsheaders',
     'fitapp',
     'fitbit_api',
     'behavioral_messages',
