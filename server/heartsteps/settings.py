e75471b65c37c0369fa2aa2a7dec885a97b57379
Nick Reid <nickreid@nickreid.com>
2018-09-19 15:40:46 -0700

    Add session authentication

--- server/heartsteps/settings.py
+++ server/heartsteps/settings.py
@@ -73,6 +73,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     )
 }
 
