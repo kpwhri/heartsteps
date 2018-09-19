71e7637def7e10a64f1c6f0eadac2881aa93e646
Peng Liao <pengliao@umich.edu>
2018-09-19 13:14:59 -0400

    Add error flag

--- activity-suggestion/app.py
+++ activity-suggestion/app.py
@@ -7,31 +7,45 @@ import subprocess
 
 app = Flask(__name__)
 
-def run_r_script(filename, request_object={}):
+def run_r_script_output(filename, request_object={}):
+  response = subprocess.run(
+    "Rscript %s '%s'" % (filename, json.dumps(request_object)),
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT,
+    universal_newlines=True)
+
+  if response.returncode > 0:
+    return response.stdout
+  else:
+    response_json = json.loads(response.stdout)
+    return json.dumps(response_json)
+
+def run_r_script_no_output(filename, request_object={}):
     response = subprocess.run(
         "Rscript %s '%s'" % (filename, json.dumps(request_object)),
         shell=True,
         stdout=subprocess.PIPE,
+	stderr=subprocess.STDOUT,
         universal_newlines=True
         )
-    try:
-        response_json = json.loads(response.stdout)
-        return json.dumps(response_json)
-    except:
-        return response.stdout, 400
-
+    if response.returncode > 0:
+       return response.stdout
+    else:
+       return "Successful Initialization/Update"
+    
 @app.route('/initialize', methods=['POST'])
 def initialize():
-    return run_r_script('initialize.r', request.json)
+    return run_r_script_no_output('initialize.r', request.json)
 
 
 @app.route('/decision', methods=['POST'])
 def decision():
-    return run_r_script('decision.r', request.json)
+    return run_r_script_output('decision.r', request.json)
 
 @app.route('/nightly', methods=['POST'])
 def nightly():
-    return run_r_script('nightly.r', request.json)
+    return run_r_script_no_output('nightly.r', request.json)
 
 
 if __name__ == "__main__":
