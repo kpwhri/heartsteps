aa8ada8c09b9052ef94bc433ffc62deb7bf93f6c
Nick Reid <nickreid@nickreid.com>
2018-09-18 18:56:50 -0700

    Add fitbit service to listen for changes to fitbit authorization

---
+++ client/src/heartsteps/fitbit.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+import { BrowserService } from "@infrastructure/browser.service";
+import { HeartstepsNotifications } from "@heartsteps/heartsteps-notifications.service";
+import { HeartstepsServer } from "@infrastructure/heartsteps-server.service";
+import { Storage } from "@ionic/storage";
+
+const storageKey: string = 'fitbit-id'
+
+@Injectable()
+export class FitbitService {
+
+    constructor(
+        private notificationService: HeartstepsNotifications,
+        private heartstepsServer: HeartstepsServer,
+        private browser: BrowserService,
+        private storage: Storage
+    ) {}
+
+    getAuthorization(heartstepsId: string):Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            const notificationSubscription = this.notificationService.onMessage().subscribe((payload: any) => {
+                if(payload.fitbit_id) {
+                    this.storage.set(storageKey, payload.fitbit_id);
+                    resolve(true);
+                } else {
+                    reject(false);
+                }
+                notificationSubscription.unsubscribe();
+            })
+
+            const fitbitAuthUrl = this.heartstepsServer.makeUrl('fitbit/authorize/user/' + heartstepsId)
+            this.browser.open(fitbitAuthUrl);
+        });
+    }
+
+    isAuthorized(): Promise<boolean> {
+        return this.storage.get(storageKey)
+        .then((fitbitId) => {
+            if(fitbitId) {
+                return true;
+            } else {
+                return Promise.reject(false);
+            }
+        });
+    }
+
+
+}
