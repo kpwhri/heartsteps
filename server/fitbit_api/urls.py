94aeb25f8c46efb929d08ab40531e30b3d8ca135
Nick Reid <nickreid@nickreid.com>
2018-09-25 16:06:45 -0700

    Fixed missing comma

--- server/fitbit_api/urls.py
+++ server/fitbit_api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from fitbit_api.views import fitbit_account, authorize, authorize_start, authorize_process, authorize_complete
 
 urlpatterns = [
-    url(r'account', fitbit_account, name='fitbit-account')
+    url(r'account', fitbit_account, name='fitbit-account'),
     url(r'authorize/generate', authorize_start, name='fitbit-authorize-start'),
     url(r'authorize/process', authorize_process, name='fitbit-authorize-process'),
     url(r'authorize/complete', authorize_complete, name='fitbit-authorize-complete'),
