0bc4f00cc12dffdc2966a7e1daecb88fc99c2944
Nick Reid <nickreid@nickreid.com>
2018-09-26 11:09:54 -0700

    Correctly read environment variables in server

--- server/heartsteps/settings.py
+++ server/heartsteps/settings.py
@@ -2,6 +2,7 @@ import os, environ
 from corsheaders.defaults import default_headers
 
 env = environ.Env()
+env.read_env('/server/.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
