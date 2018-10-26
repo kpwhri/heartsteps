bbde317b797f3d41a8617bc80333915b3c89e181
Nick Reid <nickreid@nickreid.com>
2018-10-26 14:40:38 -0700

    Split model and view tests

--- server/activity_suggestions/tests/test_views.py
+++ server/activity_suggestions/tests/test_views.py
@@ -1,46 +1,9 @@
-from unittest.mock import patch
-
-from django.test import TestCase, override_settings
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 from rest_framework.test import APITestCase
 
-from django.contrib.auth.models import User
 from activity_suggestions.models import SuggestionTime
-from django_celery_beat.models import PeriodicTask, CrontabSchedule
-
-class SuggestionTimeTestCase(TestCase):
-
-    def test_suggestion_time_creates_periodic_task(self):
-        task = SuggestionTime.objects.create(
-            user = User.objects.create(username="test"),
-            type = 'lunch',
-            hour = 15,
-            minute = 30
-        )
-
-        self.assertIsNotNone(task.scheduled_task)
-
-    def test_removes_periodic_task_when_deleted(self):
-        task = SuggestionTime.objects.create(
-            user = User.objects.create(username="test"),
-            type = 'lunch',
-            hour = 15,
-            minute = 30
-        )
-        schedules = CrontabSchedule.objects.all()
-        periodic_tasks = PeriodicTask.objects.all()
-        
-        self.assertEqual(len(periodic_tasks), 1)
-        self.assertEqual(len(schedules), 1)
-
-        task.delete()
-
-        periodic_tasks = PeriodicTask.objects.all()
-        schedules = CrontabSchedule.objects.all()
-
-        self.assertEqual(len(periodic_tasks), 0)
-        self.assertEqual(len(schedules), 0)
 
 class SuggestionTimeUpdateView(APITestCase):
 
