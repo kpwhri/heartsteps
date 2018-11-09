8c32ec2ec38aba5377658bb28b3ad85b85430253
Nick Reid <nickreid@nickreid.com>
2018-11-08 17:18:54 -0800

    Update broken test

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -7,7 +7,7 @@ import { MyApp } from './app.component';
 import { IonicModule } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { ParticipantService } from '@heartsteps/participant.service';
+import { ParticipantService } from '@heartsteps/participants/participant.service';
 import { AuthorizationService } from '@infrastructure/authorization.service';
 import { BackgroundService } from '@app/background.service.ts';
 import { NotificationService } from '@app/notification.service.ts';
