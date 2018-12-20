fa68fdc4a82d69c182f85fc091b8e3313443fd04
Nick Reid <nickreid@nickreid.com>
2018-12-20 11:50:25 -0800

    Import correct service so celery task registers

--- server/morning_messages/tasks.py
+++ server/morning_messages/tasks.py
@@ -1,8 +1,8 @@
 from celery import shared_task
 
-from morning_messages.services import MorningMessageService
+from morning_messages.services import MorningMessageDecisionService
 
 @shared_task
 def send_morning_message(username):
-    service = MorningMessageService(username=username)
+    service = MorningMessageDecisionService(username=username)
     service.send_message()
