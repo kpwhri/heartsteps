b18d8170da12bf14c7b2dc25082e0c99ba047344
Nick Reid <nickreid@nickreid.com>
2018-12-19 18:45:52 -0800

    Added morning message admin

--- server/morning_messages/admin.py
+++ server/morning_messages/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from behavioral_messages.admin import MessageTemplateAdmin
 from randomization.admin import DecisionAdmin
 
-from .models import MorningMessageTemplate, MorningMessageDecision
+from .models import Configuration, MorningMessageTemplate, MorningMessageDecision
+
+class MorningMessageConfigurationAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Configuration, MorningMessageConfigurationAdmin)
 
 class MorningMessageDecisionAdmin(DecisionAdmin):
     pass
