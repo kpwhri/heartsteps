a2291e85d651c17c3c0a1f37049b3000460985f8
Nick Reid <nickreid@nickreid.com>
2018-12-23 12:48:08 -0800

    Add placeholders for test functionality

--- client/src/pages/settings/settings-page.ts
+++ client/src/pages/settings/settings-page.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { WalkingSuggestionService } from "@heartsteps/walking-suggestions/walking-suggestion.service";
 import { EnrollmentService } from "@heartsteps/enrollment/enrollment.service";
 import { Router } from "@angular/router";
+import { AlertController } from "ionic-angular";
 
 
 @Component({
@@ -12,11 +13,35 @@ export class SettingsPage {
     constructor(
         private walkingSuggestionService:WalkingSuggestionService,
         private enrollmentService:EnrollmentService,
-        private router:Router
+        private router:Router,
+        private alertCtrl: AlertController
     ){}
 
     public testWalkingSuggestion() {
-        this.walkingSuggestionService.createTestDecision();
+        this.walkingSuggestionService.createTestDecision()
+        .then(() => {
+            const alert = this.alertCtrl.create({
+                title: 'Walking suggestion sending',
+                buttons: ['Ok']
+            });
+            alert.present();
+        });
+    }
+
+    public testAntisedentaryMessage() {
+        const alert = this.alertCtrl.create({
+            title: 'Not implemented',
+            buttons: ['Ok']
+        });
+        alert.present();
+    }
+
+    public testMoringMessage() {
+        const alert = this.alertCtrl.create({
+            title: 'Not implemented',
+            buttons: ['Ok']
+        });
+        alert.present();
     }
 
     public unenroll() {
