9bb4d948e970b21ea727bb9cb7bda0387fb2f67c
Nick Reid <nickreid@nickreid.com>
2019-01-10 11:51:05 -0800

    Made earned minutes optional

--- server/activity_logs/serializers.py
+++ server/activity_logs/serializers.py
@@ -21,7 +21,8 @@ class ActivityLogSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['id'] = instance.id
-        representation['earnedMinutes'] = instance.earned_minutes
+        if hasattr(instance, 'earned_minutes'):
+            representation['earnedMinutes'] = instance.earned_minutes
         return representation
 
 class FitbitDaySerializer(serializers.ModelSerializer):
