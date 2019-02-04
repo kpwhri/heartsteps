f62176ce9ebd45f2b782a61eaee906aa82bab536
Christopher Mack <christopher@mackdata.com>
2019-02-04 13:53:07 -0800

    Bootstrapping imports directly from db connection. Tests incomplete.

--- server/data_export/views.py
+++ server/data_export/views.py
@@ -3,9 +3,10 @@ from .resources import *
 
 # Create the django-import-export resource & instantialize
 def export_table(request, export_table):
+    print("Table name is " + export_table)
     export_resource = create_resource(export_table)
-    if export_resource == MODEL_NOT_FOUND:
-        response = HttpResponse(MODEL_NOT_FOUND)
+    if export_resource in EXCEPTION_FOUND:
+        response = HttpResponse(export_resource)
     else:
         dataset = export_resource().export()
         response = HttpResponse(dataset.csv, content_type='text/csv')
