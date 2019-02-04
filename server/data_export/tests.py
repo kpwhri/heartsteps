ef1eb824ea38e94777e640ff6e6d1a61113fc8c0
Christopher Mack <christopher@mackdata.com>
2019-02-04 14:39:23 -0800

    Added tests to data_export

--- server/data_export/tests.py
+++ server/data_export/tests.py
@@ -1,20 +1,20 @@
-from django.test import TestCase
+# from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+
+from rest_framework.test import APITestCase
+from rest_framework.test import force_authenticate
 
 from data_export.resources import MODEL_NOT_FOUND
 
 
-class DataExportTestCase(TestCase):
+class DataExportTestCase(APITestCase):
 
     def test_existing_table_on_step_data(self):
-        # response = self.client.get(reverse('data-export', args=['step_data']))
-        # print(reverse('data-export', args=['step_data']))
-        # print(response.content)
-        # self.assertEqual(response.status_code, 200)
-        pass
+        response = self.client.get(reverse('data-export', args=['step_counts']))
+        step_count_header = b'id,user,step_number,step_dtm,created\r\n'
+        self.assertEqual(response.content, step_count_header)
 
     def test_missing_table(self):
-        # response = self.client.get(reverse('data-export', args=['fake_table']))
-        # self.assertEqual(str(response.content), f"b'{MODEL_NOT_FOUND}'")
-        pass
+        response = self.client.get(reverse('data-export', args=['fake_table']))
+        self.assertEqual(str(response.content), f"b'{MODEL_NOT_FOUND}'")
--- server/data_export/views.py
+++ server/data_export/views.py
@@ -3,7 +3,6 @@ from .resources import *
 
 # Create the django-import-export resource & instantialize
 def export_table(request, export_table):
-    print("Table name is " + export_table)
     export_resource = create_resource(export_table)
     if export_resource in EXCEPTION_FOUND:
         response = HttpResponse(export_resource)
