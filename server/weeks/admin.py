8b9071adc61597c8e20c4717a7af7e1b8f4d5eec
Nick Reid <nickreid@nickreid.com>
2019-03-01 09:51:07 -0800

    Organize admin list

--- server/weeks/admin.py
+++ server/weeks/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from .models import Week
 
 class WeekAdmin(admin.ModelAdmin):
-    pass
+    ordering = ['-start_date']
+    list_display = ['user', 'number', 'start_date', 'end_date']
+    readonly_fields = ['uuid', 'user', 'number', 'start_date', 'end_date']
 admin.site.register(Week, WeekAdmin)
