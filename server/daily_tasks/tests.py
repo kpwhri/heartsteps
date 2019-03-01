486c5f8f5d587d4de41e1049cf054768f01fd9eb
Nick Reid <nickreid@nickreid.com>
2019-02-28 17:57:44 -0800

    Handle day of week when changing timezone

--- server/daily_tasks/tests.py
+++ server/daily_tasks/tests.py
@@ -18,20 +18,28 @@ class DailyTaskUpdateTest(TestCase):
         self.timezone = timezone_patch.start()
         self.timezone.return_value = pytz.timezone('Etc/GMT+7')
 
-    def test_updates_tasks_with_timezone(self):
-        DailyTask.create_daily_task(
+    def create_task(self, minute=0, hour=0, day=None):
+        return DailyTask.create_daily_task(
             user = User.objects.create(username="test"),
             category = 'example',
             task = 'example.example.task',
             name = 'Name of task!',
             arguments = {},
-            hour = 2,
-            minute = 0
+            day = day,
+            hour = hour,
+            minute = minute
+        )
+
+    def test_updates_tasks_with_timezone(self):
+        self.create_task(
+            minute = 0,
+            hour = 2
         )
 
         task = PeriodicTask.objects.get()
         # hour was set to 2, timezone is +7 == 9 GMT
         self.assertEqual(task.crontab.hour, '9')
+        self.assertEqual(task.crontab.minute, '0')
 
         self.timezone.return_value = pytz.timezone('Etc/GMT+4')
         timezone_updated.send(User, username="test")
@@ -39,3 +47,33 @@ class DailyTaskUpdateTest(TestCase):
         task = PeriodicTask.objects.get()
         # hour was set to 2, timezone is +4
         self.assertEqual(task.crontab.hour, '6')
+        self.assertEqual(task.crontab.minute, '0')
+
+    def test_registers_tasks_for_specific_day(self):
+        self.timezone.return_value = pytz.timezone('Etc/GMT-8')
+        self.create_task(
+            minute = 30,
+            hour = 20,
+            day = 'friday'
+        )
+
+        task = PeriodicTask.objects.get()
+        self.assertEqual(task.crontab.day_of_week, '5')
+
+    def test_regiesters_correct_day_with_timezone_change(self):
+        self.timezone.return_value = pytz.timezone('UTC')
+        self.create_task(
+            hour = 6,
+            day = 'sunday'
+        )
+
+        task = PeriodicTask.objects.get()
+        self.assertEqual(task.crontab.day_of_week, '0')
+        self.assertEqual(task.crontab.hour, '6')
+
+        self.timezone.return_value = pytz.timezone('Etc/GMT-8')
+        timezone_updated.send(User, username="test")
+
+        task = PeriodicTask.objects.get()
+        self.assertEqual(task.crontab.day_of_week, '6')
+        self.assertEqual(task.crontab.hour, '22')
