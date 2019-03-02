1495b09d204d15ef060582657113358c483b82d4
Nick Reid <nickreid@nickreid.com>
2019-03-01 17:34:36 -0800

    Fixed lazy coding

--- server/anti_sedentary/views.py
+++ server/anti_sedentary/views.py
@@ -1,15 +1,10 @@
 from django.utils import timezone
 
-
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
 
-from locations.services import LocationService
-
-from .models import WalkingSuggestionDecision, SuggestionTime
 from .services import AntiSedentaryDecisionService
-from .tasks import make_decision
 
 class AntiSedentaryMessageCreateView(APIView):
 
