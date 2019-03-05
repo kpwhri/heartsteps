a561d7c2241bcd5ba9061ba506f0c9bd31c573ee
Nick Reid <nickreid@nickreid.com>
2019-03-05 11:16:09 -0800

    Update daily summary on dashboard init

--- client/src/pages/dashboard/dashboard.ts
+++ client/src/pages/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 import { ActivityLogService } from '@heartsteps/activity-logs/activity-log.service';
 import { DateFactory } from '@infrastructure/date.factory';
 import { WeeklySurveyService, WeeklySurvey } from '@heartsteps/weekly-survey/weekly-survey.service';
+import { DailySummaryService } from '@heartsteps/daily-summaries/daily-summary.service';
 
 @IonicPage()
 @Component({
@@ -13,19 +14,24 @@ import { WeeklySurveyService, WeeklySurvey } from '@heartsteps/weekly-survey/wee
         DateFactory
     ]
 })
-export class DashboardPage {
+export class DashboardPage implements OnInit {
 
     public weeklySurvey:WeeklySurvey;
     public today:Date;
 
     constructor(
-        private weeklySurveyService: WeeklySurveyService
+        private weeklySurveyService: WeeklySurveyService,
+        private dailySummaryService: DailySummaryService
     ) {
         this.today = new Date();
         this.weeklySurveyService.checkExpiration();
         this.weeklySurveyService.survey.subscribe((survey) => {
             this.weeklySurvey = survey;
         });
+    }
+
+    ngOnInit() {
+        this.dailySummaryService.update(new Date());
     }
 
     public navigateToWeeklySurvey() {
