d91a84f6eff87c355bf210584febce446b223f85
Nick Reid <nickreid@nickreid.com>
2019-03-06 12:03:54 -0800

    Remove weekly goals app

--- server/weekly_goals/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .views import GoalsListView, GoalView
-
-urlpatterns = [
-    url(r'weeks/(?P<week_number>\d+)/goal', GoalView.as_view(), name='weekly-goal'),
-    url(r'weeks/goals', GoalsListView.as_view(), name='weekly-goals-list')
-]
