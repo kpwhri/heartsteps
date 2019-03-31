8688ac34526dae8c53c54e6ac6efbdf40fd58741
Nick Reid <nickreid@nickreid.com>
2019-03-31 14:05:58 -0700

    Remove spaces from activity type names

--- server/activity_types/tests.py
+++ server/activity_types/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import ActivityType
+
+class ActivityTypeTests(TestCase):
+
+    def test_remove_spaces_from_name(self):
+        ActivityType.objects.create(
+            name = "activity with spaces",
+            title= "Activity, with spaces"
+        )
+
+        activity_type = ActivityType.objects.get()
+        self.assertEqual(activity_type.name, 'activity_with_spaces')
+        self.assertEqual(activity_type.title, 'Activity, with spaces')
