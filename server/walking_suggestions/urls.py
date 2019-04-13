0bca10f3d93e1a47256b06a4100ffa4dca43e25f
Nick Reid <nickreid@nickreid.com>
2019-04-13 11:57:21 -0700

    Remove request context urls from server

--- server/walking_suggestions/urls.py
+++ server/walking_suggestions/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-from .views import WalkingSuggestionContextView, WalkingSuggestionCreateView
+from .views import WalkingSuggestionCreateView
 
 urlpatterns = [
-    url(r'(?P<decision_id>[\w\-]+)', WalkingSuggestionContextView.as_view(), name='walking-suggestions-context'),
     url(r'', WalkingSuggestionCreateView.as_view(), name='walking-suggestions-create')
 ]
