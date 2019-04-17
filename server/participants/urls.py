874b15e46949538cc11d8c74a2a9b222c232c74f
Nick Reid <nickreid@nickreid.com>
2019-04-17 13:29:42 -0700

    Fix typo

--- server/participants/urls.py
+++ server/participants/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'login', views.LoginView.as_view(), name='participants-login')
+    url(r'login', views.LoginView.as_view(), name='participants-login'),
     url(r'enroll', views.EnrollView.as_view(), name='participants-enroll')
 ]
