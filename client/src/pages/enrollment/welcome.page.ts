9668a956c502e64adfba5fbb4c8bbb8368ad9389
Nick Reid <nickreid@nickreid.com>
2019-04-26 12:37:03 -0700

    Add skeleton for website enrollment

---
+++ client/src/pages/enrollment/welcome.page.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+
+@Component({
+    templateUrl: './welcome.page.html'
+})
+export class WelcomePageComponent {
+
+    constructor(
+        private router: Router
+    ) {}
+
+    public startEnrollment() {
+        
+    }
+
+}
--- client/src/website/app.module.ts
+++ client/src/website/app.module.ts
@@ -1,23 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { IonicApp, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
-import { HeartstepsWebsite } from './app.component';
-
-import { WelcomePageModule } from '@pages/welcome/welcome.module';
-import { OnboardPageModule } from '@pages/onboard/onboard.module';
-import { HomePageModule } from '@pages/home/home.module';
-import { NotificationsModule as NotificationsPageModule } from '@pages/notifications/notifications.module';
-import { NotificationsModule } from '@heartsteps/notifications/notifications.module';
-import { WeeklySurveyModule } from '@pages/weekly-survey/weekly-survey.module';
-import { MorningSurveyPageModule } from '@pages/morning-survey/morning-survey.module';
-import { CurrentWeekModule } from '@heartsteps/current-week/current-week.module';
-import { HeartstepsInfrastructureModule } from '@infrastructure/heartsteps/heartsteps.module';
 import { AnalyticsService } from '@infrastructure/heartsteps/analytics.service';
+import { HeartstepsWebsite } from './app.component';
 
 const appRoutes:Routes = [{
   path: '',
@@ -30,16 +17,6 @@ const appRoutes:Routes = [{
     HeartstepsWebsite
   ],
   imports: [
-    WelcomePageModule,
-    CurrentWeekModule,
-    OnboardPageModule,
-    HomePageModule,
-    HeartstepsInfrastructureModule,
-    NotificationsModule,
-    NotificationsPageModule,
-    WeeklySurveyModule,
-    MorningSurveyPageModule,
-    BrowserAnimationsModule,
     IonicModule.forRoot(HeartstepsWebsite),
     RouterModule.forRoot(
       appRoutes,
@@ -53,8 +30,6 @@ const appRoutes:Routes = [{
     HeartstepsWebsite
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
     AnalyticsService
   ]
 })
