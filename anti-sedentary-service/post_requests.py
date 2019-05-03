9276207cae3de3f54690867ec7b85bb00c50e4b6
Nick Reid <nickreid@nickreid.com>
2019-05-03 16:09:23 -0700

    Add multiple calls to decision endpoint

--- anti-sedentary-service/post_requests.py
+++ anti-sedentary-service/post_requests.py
@@ -3,21 +3,52 @@ import json
 
 
 url = "http://localhost:5001/decision"
-payload = {
-    "userid": [ 1 ],
-    "decisionid": [ 1803312 ] ,
-    "time": [ "2018-10-12 10:10" ] ,
-    "daystart": [ "2018-10-12 8:00" ] ,
-    "dayend": [ "2018-10-12 20:00" ] ,
-    "state": [ 1 ],
-    "steps": [ 10 ],
-    "available": [ 1 ]
-}
+test_payloads = [
+    {
+        "userid": "test-user",
+        "decisionid": "0ad3546a-e010-4def-865c-60405a2cf623",
+        "time": "2019-05-03 10:00",
+        "daystart": "2019-05-03 08:00",
+        "dayend": "2019-05-03 20:00",
+        "state": 1,
+        "available": 1,
+        "steps": 0
+    },
+    {
+        "userid": "test-user",
+        "decisionid": "17d523c9-8904-4562-8c76-46666403d741",
+        "time": "2019-05-03 10:05",
+        "daystart": "2019-05-03 08:00",
+        "dayend": "2019-05-03 20:00",
+        "state": 1,
+        "available": 1,
+        "steps": 7,
+    },
+    {
+        "userid": "test-user",
+        "decisionid": "af4920f0-afd6-4440-a5fc-a1f951307160",
+        "time": "2019-05-03 10:10",
+        "daystart": "2019-05-03 08:00",
+        "dayend": "2019-05-03 20:00",
+        "state": 1,
+        "available": 1,
+        "steps": 0
+    },
+    {
+        "userid": "test-user",
+        "decisionid": "dba4b6d0-3138-4fc3-a394-bac2b1a301e3",
+        "time": "2019-05-03 10:15",
+        "daystart": "2019-05-03 08:00",
+        "dayend": "2019-05-03 20:00",
+        "state": 1,
+        "available": 1,
+        "steps": 0
+    }
+]
 
-
-r = requests.post(url, json=payload)
-print(r.text)
-
+for payload in test_payloads:
+    r = requests.post(url, json=payload)
+    print(r.text)
 
 url = "http://localhost:5001/nightly"
 payload = {
