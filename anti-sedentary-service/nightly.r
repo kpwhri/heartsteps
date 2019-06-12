87f49131b40ea04a37056a023f7854fad932b40d
wdempsey <dempsey.walter@gmail.com>
2019-06-12 12:12:57 -0400

    Fixed nightly to return proper append (T/F were flipped) AND to fix the time when appending new row

--- anti-sedentary-service/nightly.r
+++ anti-sedentary-service/nightly.r
@@ -51,17 +51,21 @@ if ( any(is.element(user.data$time,current.time)) ) {
     obs.row = which(is.element(user.data$time,current.time))
     user.data$batch_state[obs.row] = input$state
     user.data$batch_step[obs.row] = input$steps
-    results <- list(append = TRUE)
+    results <- list(append = FALSE)
     write.csv(user.data, file = file_name, row.names = FALSE)
 } else {
-    ## IF ROW DID NOT EXIST THEN APPEND ROW
-    temp.data = data.frame(userid = input$userid, decisionid = input$decisionid,
-                           time = input$time, daystart = input$daystart, dayend = input$dayend,
-                           online_state = -1, online_step = -1, available = 0,
-                           batch_state = input$state, batch_step = input$steps, probaction = 0.0, action = 0.0,
-                           missingindicator = 1, duplicate = FALSE)
-    results <- list(append = FALSE)
-    write.csv(rbind(user.data, temp.data), file = file_name, row.names = FALSE)
+  ## IF ROW DID NOT EXIST THEN APPEND ROW
+  current.time = as.POSIXct(strptime(input$time, "%Y-%m-%d %H:%M"), tz = "Etc/GMT+6")
+  final.time = as.POSIXct(strptime(input$dayend, "%Y-%m-%d %H:%M"), tz = "Etc/GMT+6")
+  beginning.time = as.POSIXct(strptime(input$daystart, "%Y-%m-%d %H:%M"), tz = "Etc/GMT+6")
+  
+  temp.data = data.frame(userid = input$userid, decisionid = input$decisionid,
+                         time = current.time, daystart =beginning.time, dayend = final.time,
+                         online_state = -1, online_step = -1, available = 0,
+                         batch_state = input$state, batch_step = input$steps, probaction = 0.0, action = 0.0,
+                         missingindicator = 1, duplicate = FALSE)
+  results <- list(append = FALSE)
+  write.csv(rbind(user.data, temp.data), file = file_name, row.names = FALSE)
 }
 
 # output the results
