7aaad116f7f10298a502ff68c48faafbda57a795
Nick Reid <nickreid@nickreid.com>
2019-06-16 15:59:53 -0700

    Update data export to make multiple queues, rather then upload all participant files at once

--- server/heartsteps_data_download/tasks.py
+++ server/heartsteps_data_download/tasks.py
@@ -18,18 +18,12 @@ from walking_suggestions.models import WalkingSuggestionServiceRequest
 
 EXPORT_DIRECTORY = '/heartsteps-export'
 
-def write_csv_file(filename, content):
-    file = open(
-        '%s/%s' % (
-            EXPORT_DIRECTORY,
-            filename
-        ),
-        'w'
-    )
+def write_csv_file(directory, filename, content):
+    file = open(os.path.join(directory, filename), 'w')
     file.write(content)
     file.close()
 
-def export_walking_suggestion_decisions(username):
+def export_walking_suggestion_decisions(username, directory):
     print('- walking suggestion decisions')
     try:
         configuration = Configuration.objects.get(user__username = username)
@@ -48,11 +42,12 @@ def export_walking_suggestion_decisions(username):
         )
     )
     write_csv_file(
+        directory = directory,
         filename = '%s.walking_suggestion_decisions.csv' % (username),
         content = dataset.csv
     )
 
-def export_walking_suggestion_service_requests(username):
+def export_walking_suggestion_service_requests(username, directory):
     print('- walkling suggestion service_requests')
     dataset = ServiceRequestResource().export(
         queryset = WalkingSuggestionServiceRequest.objects.filter(
@@ -60,11 +55,12 @@ def export_walking_suggestion_service_requests(username):
         )
     )
     write_csv_file(
+        directory = directory,
         filename = '%s.walking_suggestion_service_requests.csv' % (username),
         content = dataset.csv
     )
 
-def export_anti_sedentary_decisions(username):
+def export_anti_sedentary_decisions(username, directory):
     print('- anti-sedentary decisions')
     dataset = AntiSedentaryDecisionResouce().export(
         queryset = AntiSedentaryDecision.objects.filter(
@@ -72,11 +68,12 @@ def export_anti_sedentary_decisions(username):
         )
     )
     write_csv_file(
+        directory = directory,
         filename = '%s.anti_sedentary_decisions.csv',
         content = dataset.csv
     )
 
-def export_anti_sedentary_service_requests(username):
+def export_anti_sedentary_service_requests(username, directory):
     print('- anti-sedentary service requests')
     dataset = ServiceRequestResource().export(
         queryset = AntiSedentaryServiceRequest.objects.filter(
@@ -84,26 +81,36 @@ def export_anti_sedentary_service_requests(username):
         )
     )
     write_csv_file(
+        directory = directory,
         filename = '%s.anti_sedentary_service_requests' % (username),
         content = dataset.csv
-    )  
-
+    )
 
 @shared_task
-def download_data():
+def export_user_data(username):
     if not hasattr(settings, 'HEARTSTEPS_NIGHTLY_DATA_BUCKET') or not settings.HEARTSTEPS_NIGHTLY_DATA_BUCKET:
         print('Data download not configured')
         return False
     if not os.path.exists(EXPORT_DIRECTORY):
         os.makedirs(EXPORT_DIRECTORY)
-    for participant in Participant.objects.exclude(user=None).all():
-        print(participant.user.username)
-        export_walking_suggestion_decisions(participant.user.username)
-        export_walking_suggestion_service_requests(participant.user.username)
-        # export_anti_sedentary_decisions(participant.user.username)
-        export_anti_sedentary_service_requests(participant.user.username)
+    user_directory = os.path.join(EXPORT_DIRECTORY, username)
+    if not os.path.exists(user_directory):
+        os.makedirs(user_directory)
+    print(username)
+    export_walking_suggestion_decisions(username=username, directory=user_directory)
+    export_walking_suggestion_service_requests(username=username, directory=user_directory)
+    # export_anti_sedentary_decisions(username=username, directory=user_directory)
+    export_anti_sedentary_service_requests(username=username, directory=user_directory)
+    
     subprocess.call(
-        'gsutil -m rsync -r %s gs://%s' % (EXPORT_DIRECTORY, settings.HEARTSTEPS_NIGHTLY_DATA_BUCKET),
+        'gsutil -m rsync %s gs://%s' % (user_directory, settings.HEARTSTEPS_NIGHTLY_DATA_BUCKET),
         shell=True
     )
 
+
+@shared_task
+def download_data():
+    for participant in Participant.objects.exclude(user=None).all():
+        export_user_data.apply_async(kwargs={
+            'username': participant.user.username
+        })
