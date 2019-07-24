ca548ae23482fe0a99edad16ba3cb9799241a226
Nick Reid <nickreid@nickreid.com>
2019-07-24 12:22:51 -0700

    Update adherence message admin

--- server/adherence_messages/admin.py
+++ server/adherence_messages/admin.py
@@ -1,7 +1,58 @@
+from django.conf import settings
 from django.contrib import admin
 
-from .models import AdherenceMetric
+from days.services import DayService
+
 from .models import Configuration
+from .models import AdherenceMessage
+from .models import AdherenceMetric
+
+class ConfigurationAdmin(admin.ModelAdmin):
+    fields = [
+        'user',
+        'enabled',
+        'daily_update'
+    ]
+
+    readonly_fields = [
+        'daily_update'
+    ]
+
+    ordering = ['user__username']
+
+    def daily_update(self, instance):
+        if instance.daily_task:
+            next_run_datetime = instance.daily_task.get_next_run_time()
+            day_service = DayService(user = instance.user)
+            timezone = day_service.get_timezone_at(next_run_datetime)
+            corrected_datetime = next_run_datetime.astimezone(timezone)
+            if next_run_datetime:
+                return 'Next update at %s (%s)' % (corrected_datetime.strftime('%Y-%m-%d %H:%M'), timezone.zone)
+        else:
+            return 'No daily update'
+
+admin.site.register(Configuration, ConfigurationAdmin)
+
+class AdherenceMessageAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'created',
+        'category',
+        'body'
+    ]
+    fields = [
+        'user',
+        'category',
+        'body'
+    ]
+    readonly_fields = [
+        'user',
+        'category',
+        'body'
+    ]
+    ordering = ['-created', 'user__username']
+
+admin.site.register(AdherenceMessage, AdherenceMessageAdmin)
 
 class AdherenceMetricAdmin(admin.ModelAdmin):
     list_display = [
@@ -12,10 +63,3 @@ class AdherenceMetricAdmin(admin.ModelAdmin):
     ordering = ['-date', 'user__username']
 
 admin.site.register(AdherenceMetric, AdherenceMetricAdmin)
-
-class ConfigurationAdmin(admin.ModelAdmin):
-    fields = [
-        'user',
-        'enabled'
-    ]
-admin.site.register(Configuration, ConfigurationAdmin)
