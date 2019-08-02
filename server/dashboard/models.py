c26cb48be4f9eb95b8679fac8dedc5df0e0466c1
Christopher Mack <christopher@mackdata.com>
2019-08-01 17:06:53 -0700

    Rewiring Dashboard to use new adherence definitions

--- server/dashboard/models.py
+++ server/dashboard/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+from adherence_messages.services import (
+    AdherenceAppInstallMessageService,
+    AdherenceFitbitUpdatedService
+)
+from fitbit_api.models import (
+    FitbitAccount,
+    FitbitAccountUser#,
+    #FitbitSubscriptionUpdate
+)
+from fitbit_api.services import (
+    FitbitService  #,
+    # FitbitSubscriptionUpdate
+)
+
+
+class AdherenceAppInstallDashboard(AdherenceAppInstallMessageService):
+
+    def __init__(self, user=None):
+        self._user = user
+
+
+# class AdherenceFitbitUpdatedDashboard(AdherenceFitbitUpdatedService):
+
+#     def __init__(self, user=None):
+#         self._user = user
+
+
+class FitbitServiceDashboard(FitbitService):
+
+    def __init__(self, user=None):
+        # Need to bring in _FitbitService__account
+        # Original method uses __account which resolves to the above
+        try:
+            super(FitbitServiceDashboard, self).__init__(user=user)
+            self._FitbitService__account = self.account
+        except FitbitService.NoAccount:
+            self.account = None
+            self._FitbitService__account = None
+            self.__user = user
+        # print("Init acct: " + str(self.__account))
+
+    def get_account(user):
+        try:
+            super(FitbitServiceDashboard, self).get_account()
+        except FitbitServiceDashboard.NoAccount:
+            return None
+
+    def last_updated_on(self):
+        try:
+            super(FitbitServiceDashboard, self).last_updated_on()
+            x = super(FitbitServiceDashboard, self).last_updated_on()
+            print("Updated!")
+            print("x: " + str(x))
+            print("Date: " + str(self.last_updated_on()))
+        except FitbitServiceDashboard.AccountNeverUpdated:
+            return None
--- server/dashboard/views.py
+++ server/dashboard/views.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.models import User
-from django.db.models import Case, IntegerField, Sum, When
+# from django.db.models import Case, IntegerField, Sum, When
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import TemplateView
 
 from contact.models import ContactInformation
 from fitbit_activities.models import FitbitActivity, FitbitDay
@@ -13,12 +13,17 @@ from participants.models import Participant
 from sms_service.forms import SendSMSForm
 from sms_service.views import SendSmsCreateView
 
+# from adherence_messages.services import AdherenceAppInstallMessageService
+from .models import AdherenceAppInstallDashboard
+# from .models import AdherenceFitbitUpdatedDashboard
+from .models import FitbitServiceDashboard
 
-class DashboardListView(UserPassesTestMixin, ListView):
 
-    model = Participant
-    queryset = Participant.objects.all().prefetch_related(
-        'user').order_by('heartsteps_id')
+class DashboardListView(UserPassesTestMixin, TemplateView):
+
+    # model = Participant
+    # queryset = Participant.objects.all().prefetch_related(
+    #     'user').order_by('heartsteps_id')
     template_name = 'dashboard/index.html'
 
     def test_func(self):
@@ -33,6 +38,21 @@ class DashboardListView(UserPassesTestMixin, ListView):
         context = super(DashboardListView, self).get_context_data(**kwargs)
         context['from_number'] = settings.TWILIO_PHONE_NUMBER
         context['form'] = SendSMSForm
+
+        participants = []
+        for participant in Participant.objects.all().prefetch_related(
+                                      'user').order_by('heartsteps_id'):
+            adherence_app_install = AdherenceAppInstallDashboard(
+                                    user=participant.user)
+            fitbit_service = FitbitServiceDashboard(user=participant.user)
+            participants.append({
+                'heartsteps_id': participant.heartsteps_id,
+                'enrollment_token': participant.enrollment_token,
+                'birth_year': participant.birth_year,
+                'days_wore_fitbit': adherence_app_install.days_wore_fitbit,
+                'last_updated_on': fitbit_service.last_updated_on()
+            })
+        context['participant_list'] = participants
         return context
 
     def post(self, request, *args, **kwargs):
