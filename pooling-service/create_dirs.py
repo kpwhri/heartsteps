8d08801813eaf3576152adde9767b23d008cb776
Sabina321 <sabina.tomkins@gmail.com>
2019-09-02 18:58:41 -0700

    write dirs

--- pooling-service/create_dirs.py
+++ pooling-service/create_dirs.py
@@ -5,4 +5,4 @@ if __name__=="__main__":
     newdir = [f+'_pooled_params' for f in users]
     for f in newdir:
         if not os.path.isdir('data/{}'.format(f)):
-            os.makedir('data/{}'.format(f))
+            os.mkdir('data/{}'.format(f))
