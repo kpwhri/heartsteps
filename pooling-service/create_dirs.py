58283a6d9fa5d681af9a0ccdc25bde1020f0f7e2
Sabina321 <sabina.tomkins@gmail.com>
2019-09-09 16:37:14 -0700

    backin up

--- pooling-service/create_dirs.py
+++ pooling-service/create_dirs.py
@@ -2,7 +2,11 @@ import os
 
 if __name__=="__main__":
     users = [f for f in os.listdir('data') if 'user' in f]
-    newdir = [f+'_pooled_params' for f in users]
+    newdir = [f+'_pooled_params' for f in users if '_pooled' not in f]
+    to_delete = [f for f in os.listdir('data') if 'pooled_params_pooled_params' in f  ]
+    for f in to_delete:
+        os.rmdir('data/{}'.format(f))
     for f in newdir:
+        
         if not os.path.isdir('data/{}'.format(f)):
             os.mkdir('data/{}'.format(f))
