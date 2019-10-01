6de7f7482ea6fd8ee8ed7775aa1d46563c9bef3b
Christopher Mack <christopher@mackdata.com>
2019-10-01 16:38:16 -0700

    Add endpoint to add participants after contact by Survey

--- server/participants/serializers.py
+++ server/participants/serializers.py
@@ -2,7 +2,14 @@ from rest_framework import serializers
 
 from .models import Participant
 
+
 class ParticipantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Participant
         fields = ('enrollment_token', 'birth_year')
+
+
+class ParticipantAddSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = ('heartsteps_id', 'cohort', 'enrollment_token', 'birth_year')
