a905c24ec13b2668318e216f54a54000deede94e
jup014 <76829735+jup014@users.noreply.github.com>
2021-03-30 15:20:44 -0700

    delete unnecessary code

--- server/dashboard/views.py
+++ server/dashboard/views.py
@@ -86,16 +86,7 @@ class DevFrontView(UserPassesTestMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        import environ
-        # print(settings.DATABASE_URL)
         
-        # root = environ.Path()
-        env = environ.Env()
-        environ.Env.read_env(".env-development")  # reading .env file
-        # print(root)
-        print(env)
-        print(env.str('ONESIGNAL_APP_ID'))
-        # print(settings.configured)
         context['cohorts'] = []
         studies = []
         if self.request.user.is_superuser:
