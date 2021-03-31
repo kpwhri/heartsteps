8e19cf97438510c17fdae09c303306e34ca2115f
Nick Reid <nickreid@nickreid.com>
2021-03-31 13:56:55 -0700

    Load activity summary from server, not computed from cached daily summaries

--- client/src/heartsteps/daily-summaries/activity-summary.service.ts
+++ client/src/heartsteps/daily-summaries/activity-summary.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
+import { DocumentStorage, DocumentStorageService } from "@infrastructure/document-storage.service";
+import { HeartstepsServer } from "@infrastructure/heartsteps-server.service";
+import { StorageService } from "@infrastructure/storage.service";
 import { BehaviorSubject } from "rxjs";
 import { DailySummaryService } from "./daily-summary.service";
-
 
 
 @Injectable()
@@ -12,21 +14,59 @@ export class ActivitySummaryService {
     public totalMiles: BehaviorSubject<number>;
     public totalSteps: BehaviorSubject<number>;
 
+
     constructor(
-        private dailySummaryService: DailySummaryService
+        private dailySummaryService: DailySummaryService,
+        private storage: StorageService,
+        private heartstepsServer: HeartstepsServer
     ) {
         this.totalActivitiesCompleted = new BehaviorSubject(undefined);
         this.totalActivityMinutes = new BehaviorSubject(undefined);
         this.totalMiles = new BehaviorSubject(undefined);
         this.totalSteps = new BehaviorSubject(undefined);
 
-        this.updateStatistics();
+        var updateDebounce = undefined;
         this.dailySummaryService.updated.subscribe(() => {
-            this.updateStatistics();
+            if (updateDebounce) clearTimeout(updateDebounce);
+            updateDebounce = setTimeout(() => {
+                this.updateStatistics();
+            }, 500);
         });
     }
 
+    private get(): Promise<void> {
+        return this.heartstepsServer.get('activity/summary')
+        .then((data) => {
+            this.storage.set('activity-summary', data);
+            return undefined;
+        });
+    }
+
+    private load(): Promise<void> {
+        return this.storage.get('activity-summary')
+        .then((data) => {
+            this.totalActivitiesCompleted.next(data['activities_completed']);
+            this.totalActivityMinutes.next(data['minutes']);
+            this.totalMiles.next(data['minutes']);
+            this.totalSteps.next(data['steps']);
+        })
+        .catch(() => {
+            this.get()
+            .then(() => {
+                this.load()
+            });
+        });
+        return Promise.resolve(undefined);
+    }
+
     public updateStatistics(): Promise<void> {
+        return this.heartstepsServer.get('activity/summary')
+        .then((data) => {
+            this.storage.set('activity-summary', data);
+            return this.load();
+        });
+
+
         return this.dailySummaryService.getAll()
         .then((dailySummaries) => {
             let totalActivitiesCompleted = 0;
