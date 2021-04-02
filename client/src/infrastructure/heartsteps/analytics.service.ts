9c50187b7580bbdbe9601d0959d05bc8915ad6d7
Nick Reid <nickreid@nickreid.com>
2021-04-02 15:01:24 -0700

    Delay page view upload to server by one second

--- client/src/infrastructure/heartsteps/analytics.service.ts
+++ client/src/infrastructure/heartsteps/analytics.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HeartstepsServer } from "@infrastructure/heartsteps-server.service";
 import { AuthorizationService } from "@infrastructure/authorization.service";
 import { Router, NavigationEnd } from "@angular/router";
+import { StorageService } from "@infrastructure/storage.service";
 
 declare var process: {
     env: {
@@ -18,10 +19,13 @@ export class AnalyticsService {
     private version: string; 
     private build: string;
 
+    private delayUploadTimeout: any;
+
     constructor(
         private heartstepsServer: HeartstepsServer,
         private authorizationService: AuthorizationService,
-        private router: Router
+        private router: Router,
+        private storage: StorageService
     ) {
         this.build = process.env.BUILD_NUMBER;
         this.platform = process.env.BUILD_PLATFORM;
@@ -54,19 +58,56 @@ export class AnalyticsService {
     public trackPageView(uri:string):Promise<boolean> {
         return this.authorizationService.isAuthorized()
         .then(() => {
-            return this.heartstepsServer.post('/page-views', {
-                uri: uri,
-                time: new Date().toISOString(),
-                platform: this.platform,
-                version: this.version,
-                build: this.build
-            });
+            const pageView = this.createPageView(uri);
+            return this.storePageView(pageView);
         })
         .then(() => {
+            this.delayUpload();
             return true;
         })
         .catch(() => {
             return Promise.resolve(false);
+        });
+    }
+
+    public uploadPageView(): Promise<void> {
+        return this.storage.get('page-views')
+        .then((pageViews) => {
+            return this.heartstepsServer.post('page-views', pageViews)
+        })
+        .then(() => {
+            this.storage.remove('page-views');
+        });
+    }
+
+    private delayUpload() {
+        if (this.delayUploadTimeout) clearTimeout(this.delayUploadTimeout);
+        this.delayUploadTimeout = setTimeout(() => {
+            this.uploadPageView()
+        }, 1000);
+    }
+
+    private createPageView(uri: string): any {
+        return {
+            uri: uri,
+            time: new Date().toISOString(),
+            platform: this.platform,
+            version: this.version,
+            build: this.build
+        }
+    }
+
+    private storePageView(pageView:any):Promise<void> {
+        return this.storage.get('page-views')
+        .catch(() => {
+            return [];
+        })
+        .then((pageViews) => {
+            if (!Array.isArray(pageViews)) {
+                pageViews = []
+            }
+            pageViews.push(pageView);
+            return this.storage.set('page-views', pageViews);
         });
     }
 
