6e63daefb0b6e13c2e5b9b29fdadfd070beb3b07
Nick Reid <nickreid@nickreid.com>
2021-05-12 19:15:32 -0700

    Remove EnrollmentView and simplify LoginViews

--- server/participants/urls.py
+++ server/participants/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from .views import (LoginView, LogoutView, EnrollView,
-                    ParticipantInformationView, ParticipantAddView)
+from .views import LoginView
+from .views import LogoutView
+from .views import ParticipantInformationView
+from .views import ParticipantAddView
 
 urlpatterns = [
     url(r'login', LoginView.as_view(), name='participants-login'),
     url(r'logout', LogoutView.as_view(), name='participants-logout'),
-    url(r'enroll', EnrollView.as_view(), name='participants-enroll'),
+    url(r'enroll', LoginView.as_view(), name='participants-enroll'),
     url(r'information', ParticipantInformationView.as_view(),
         name='participants-information'),
     url(r'participant-add', ParticipantAddView.as_view(),
