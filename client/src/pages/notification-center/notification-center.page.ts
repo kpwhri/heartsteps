be7cfa85cf5f148a779ff0f5bc20327fabbee596
patrick9958 <patrick.neggie@gmail.com>
2021-05-31 19:29:53 -0400

    added notification-center page skeleton code

---
+++ client/src/pages/notification-center/notification-center.page.ts
@@ -0,0 +1,7 @@
+// import { Component } from "@angular/core";
+// // import { ActivatedRoute, Router } from "@angular/router";
+
+// @Component({
+//     templateUrl: "./notification-center.page.html",
+// })
+// export class NotificationCenterPage {}
