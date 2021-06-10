d51c1d9f1e278f6495a63ba511b4c74602ed8fc1
Rahul D'Costa <rdcosta@umich.edu>
2021-06-10 19:28:26 +0400

    #208 Basic imports and properties added to StepGoals model

--- server/daily_step_goals/models.py
+++ server/daily_step_goals/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
+from datetime import date
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 # Create your models here.
 class StepGoals(models.Model):
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
     date = models.DateField()
-    step_goal = models.Integer
+    step_goal = models.PositiveIntegerField()
+
+    @property
+    def id(self):
+        return str(self.uuid)
