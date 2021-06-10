a3c4b399b3eb74681355b3cf5f2c965234adbe65
Rahul D'Costa <rdcosta@umich.edu>
2021-06-10 19:55:10 +0400

    #208 views updated for saving data (step goal + date) in DB table

--- server/daily_step_goals/views.py
+++ server/daily_step_goals/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from datetime import datetime
+
+from django.http import Http404
+
+from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+
+from .serializers import ActivityLogSerializer, TimeRangeSerializer
+from .models import StepGoals
+
+def insertSteps():
+    daily_step_goal_log = StepGoals()
+
+    daily_step_goal_log.step_goal = 8000
+    daily_step_goal_log.date = datetime.today()
+    daily_step_goal_log.save()
+
