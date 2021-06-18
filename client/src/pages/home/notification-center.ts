2215e4914f9e972b33b760e55f03eddcb09e124a
patrick9958 <patrick.neggie@gmail.com>
2021-06-17 20:56:21 -0400

    #215 implemented basic logic of read/unread

--- client/src/pages/home/notification-center.ts
+++ client/src/pages/home/notification-center.ts
@@ -13,12 +13,36 @@ export class NotificationCenterPage implements OnInit {
         this.notifications;
     }
 
+    // pull notifications from django
     private getNotifications(): Promise<Notification[]> {
         return this.notificationService
             .getRecentNotifications()
             .then((notifications) => {
                 return (this.notifications = notifications);
             });
+    }
+
+    // checks to see if user has seen the push notification
+    public isRead(notification: Notification): boolean {
+        if (this.isReceived(notification)) {
+            /* 
+            engaged means user dismissed notification from their OS notification center
+            opened means user opened push notification and it launched heartsteps app 
+            */
+            if (notification.engaged || notification.opened) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    // checks to see if user got a push notification
+    public isReceived(notification: Notification): boolean {
+        if (notification.sent && notification.received) {
+            return true;
+        }
+        return false;
     }
 
     ngOnInit() {
