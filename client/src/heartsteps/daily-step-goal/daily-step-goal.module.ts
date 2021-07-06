53e761ab6a86ed8bd7881bfcf4f545376c9ec651
Rahul D'Costa <rdcosta@umich.edu>
2021-07-06 21:08:07 +0400

    Revert "#208 created daily-step-goal component"
    This reverts commit b256a38c

--- client/src/heartsteps/daily-step-goal/daily-step-goal.module.ts
+++ client/src/heartsteps/daily-step-goal/daily-step-goal.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { InfrastructureModule } from '@infrastructure/infrastructure.module';
+
 import { DailyStepGoalComponent } from './daily-step-goal.component'
 
 @NgModule({
