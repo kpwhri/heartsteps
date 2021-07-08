6778a844ec3f2a14dfb07b537de0457ce9522158
Rahul D'Costa <rdcosta@umich.edu>
2021-07-09 01:12:05 +0400

    #208 minor fix to update function

--- client/src/heartsteps/daily-step-goal/daily-step-goal.component.ts
+++ client/src/heartsteps/daily-step-goal/daily-step-goal.component.ts
@@ -25,7 +25,6 @@ export class DailyStepGoalComponent {
         .catch(() => {
             console.log('Daily step count goal failed')
         })
-        this.dailyStepGoal = this.summary.minutes;
     }
 }
 
