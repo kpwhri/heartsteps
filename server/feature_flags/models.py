32400eb48a121b0b87ee63bb3903d0b128e68ba0
patrick9958 <patrick.neggie@gmail.com>
2021-07-11 19:42:24 -0400

    #224 added FeatureFlags model

--- server/feature_flags/models.py
+++ server/feature_flags/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 import uuid
 
 
-# class FeatureFlag(models.Model):
-#     uuid = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4)
-#     user = models.ForeignKey(User, on_delete = models.CASCADE)
-#     notification_center_flag = models.BooleanField(default=False)
+class FeatureFlags(models.Model):
+    uuid = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    notification_center_flag = models.BooleanField(default=False)
 
-#     def __str__(self):
-#         return self.user.username
+    def __str__(self):
+        return self.user.username
