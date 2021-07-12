c2820474f9747367af199ace706e839080ae9edf
patrick9958 <patrick.neggie@gmail.com>
2021-07-11 23:25:44 -0400

    #224 API URL working

--- server/feature_flags/models.py
+++ server/feature_flags/models.py
@@ -4,6 +4,10 @@ import uuid
 
 
 class FeatureFlags(models.Model):
+    class Meta:
+        verbose_name = 'Feature Flags'
+        verbose_name_plural = 'Feature Flags'
+
     uuid = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
     notification_center_flag = models.BooleanField(default=False)
--- server/feature_flags/views.py
+++ server/feature_flags/views.py
@@ -1,15 +1,28 @@
 from rest_framework.views import APIView
 from rest_framework import status, permissions
 from rest_framework.response import Response
+from django.http.response import Http404
 
 from .serializers import FeatureFlagsSerializer
 from .models import FeatureFlags
 
 class FeatureFlagsList(APIView):
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get_feature_flags(self):
-        return []
+    # TODO: re-enable, only for testing purposes
+    # permission_classes = (permissions.IsAuthenticated,)
 
     def get(self, request):
-        return Response({}, status=status.HTTP_400_BAD_REQUEST)
+        # check to see if the request is allowed (i.e. participant is logged in)
+        if not request.user or request.user.is_anonymous:
+            # TODO: remove, only for testing purposes
+            feature_flags = FeatureFlags.objects.filter(user__username='test').first()
+            serialized = FeatureFlagsSerializer(feature_flags)
+            return Response(serialized.data, status=status.HTTP_200_OK)
+            return Response({"No participant, please log in"}, status.HTTP_400_BAD_REQUEST)
+
+        feature_flags = FeatureFlags.objects.filter(user=request.user).first()
+
+        if not feature_flags or feature_flags == []:
+            return Response({"No feature flags object for user"}, status.HTTP_400_BAD_REQUEST)
+
+        serialized = FeatureFlagsSerializer(feature_flags)
+        return Response(serialized.data, status=status.HTTP_200_OK)
