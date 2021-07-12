08f8aa345e5c9120860fa47f05d4c2da77a1f9cb
patrick9958 <patrick.neggie@gmail.com>
2021-07-11 22:56:22 -0400

    #224 New Django migration
    creates new FeatureFlag object for all existing users
    that don't have a FeatureFlags object linked

--- server/feature_flags/models.py
+++ server/feature_flags/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 import uuid
 
 
 class FeatureFlags(models.Model):
     uuid = models.CharField(max_length=50, primary_key=True, default=uuid.uuid4)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
     notification_center_flag = models.BooleanField(default=False)
 
     def __str__(self):
