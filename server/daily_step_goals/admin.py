a10b0a86ea9a4c2f44bd1aa5ddab0397d2231f50
Rahul D'Costa <rdcosta@umich.edu>
2021-07-13 14:10:35 +0400

    #230 registered Days on admin site for DSG

--- server/daily_step_goals/admin.py
+++ server/daily_step_goals/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import StepGoals
+from .models import StepGoals, Days
 
 class StepGoalsAdmin(admin.ModelAdmin):
     pass
 admin.site.register(StepGoals, StepGoalsAdmin)
+admin.site.register(Days)
