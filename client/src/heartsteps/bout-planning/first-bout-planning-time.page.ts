225b2b21bcfce924d923428d0baaabd75f123021
jup014 <76829735+jup014@users.noreply.github.com>
2021-07-27 23:37:34 -0700

    removed featureFlag from first-bout-planning-time.page.ts

--- client/src/heartsteps/bout-planning/first-bout-planning-time.page.ts
+++ client/src/heartsteps/bout-planning/first-bout-planning-time.page.ts
@@ -1,13 +1,9 @@
-import { Inject, Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 import { LoadingService } from '@infrastructure/loading.service';
 import { FirstBoutPlanningTimeService, FirstBoutPlanningTime } from './first-bout-planning-time.service';
 
-import { Subscription } from 'rxjs';
-import { skip } from 'rxjs/operators';
-import { FeatureFlags } from "@heartsteps/feature-flags/FeatureFlags";
-import { FeatureFlagService } from "@heartsteps/feature-flags/feature-flags.service";
 
 
 @Component({
@@ -17,39 +13,20 @@ import { FeatureFlagService } from "@heartsteps/feature-flags/feature-flags.serv
 export class FirstBoutPlanningTimePage {
 
   public firstBoutPlanningForm: FormGroup;
-  private featureFlagSubscription: Subscription;
 
   @Output() saved = new EventEmitter<boolean>();
 
   constructor(
     private loadingService: LoadingService,
-    private firstBoutPlanningTimeService: FirstBoutPlanningTimeService,
-    private featureFlagService: FeatureFlagService
+    private firstBoutPlanningTimeService: FirstBoutPlanningTimeService
   ) {
-    this.featureFlagSubscription = this.featureFlagService
-      .currentFeatureFlags
-      .pipe(skip(1))  // BehaviorSubject class provides the default value (in this case, an empty feature flag list). This line skip the default value
-      .subscribe(
-        (flags) => {
-          if (this.featureFlagService.hasFlag('nlm')) {
-            // handle "with flag" case
-            console.log("nlm tag is found.")
-            this.firstBoutPlanningTimeService.getTime()
+    this.firstBoutPlanningTimeService.getTime()
               .catch(() => {
                 return this.firstBoutPlanningTimeService.getDefaultFirstBoutPlanningTime()
               })
               .then((firstBoutPlanningTime: FirstBoutPlanningTime) => {
                 this.createFirstBoutPlanningForm(firstBoutPlanningTime.time);
               });
-          } else {
-            // handle "without flag" case
-            console.log("no nlm tag is found.")
-            this.saved.emit();
-          }
-          this.featureFlagSubscription.unsubscribe();
-        }
-    );
-    this.featureFlagService.getFeatureFlags();
   }
 
   createFirstBoutPlanningForm(time: Date) {
