6da85efbd7dc4033eec77303d8746eac49308359
Rahul D'Costa <rdcosta@umich.edu>
2021-08-01 19:28:23 +0400

    #231 Made migrations for new log fields

--- server/user_event_logs/signals.py
+++ server/user_event_logs/signals.py
@@ -4,7 +4,7 @@ from django.core.signals import request_finished
 from django.db.models.signals import pre_save, post_save
 
 from daily_step_goals.models import StepGoal
-from .models import AuditEntry
+from .models import AuditEntry, EventLog
 
 # The following signal receiver signals when the user logs in
 @receiver(user_logged_in)
