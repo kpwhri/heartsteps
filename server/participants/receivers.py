d853c05f57d1a9ce1f57168fb3f90cf9474dfec1
patrick9958 <patrick.neggie@gmail.com>
2021-08-09 23:14:19 -0400

    #261 fixing build errors

--- server/participants/receivers.py
+++ server/participants/receivers.py
@@ -1,14 +1,13 @@
 import participants
-from participants.models import Participant
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from feature_flags.models import FeatureFlags
-from .models import Study, Cohort, Participant
+from participants.models import Study, Cohort, Participant
 
 
 @receiver(post_save, sender=Participant)
 def create_feature_flags(sender, instance, created, **kwargs):
-    if created and not FeatureFlags.exists(user=instance.user):
+    if created and instance.cohort and instance.user and not FeatureFlags.exists(user=instance.user):
         FeatureFlags.create(
             user=instance.user, flags=instance.cohort.study.studywide_feature_flags)
 
