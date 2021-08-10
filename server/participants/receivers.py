da83fd470011d2b21877b7f0fd661668440dee38
patrick9958 <patrick.neggie@gmail.com>
2021-08-09 23:03:42 -0400

    #261 updates in studywide_featureflags propagate to all users in that study

--- server/participants/receivers.py
+++ server/participants/receivers.py
@@ -3,6 +3,7 @@ from participants.models import Participant
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from feature_flags.models import FeatureFlags
+from .models import Study, Cohort, Participant
 
 
 @receiver(post_save, sender=Participant)
@@ -10,3 +11,17 @@ def create_feature_flags(sender, instance, created, **kwargs):
     if created and not FeatureFlags.exists(user=instance.user):
         FeatureFlags.create(
             user=instance.user, flags=instance.cohort.study.studywide_feature_flags)
+
+
+@receiver(post_save, sender=Study)
+def update_feature_flags(sender, instance, created, **kwargs):
+    if not created:
+        # get cohorts inside this study
+        cohorts = Cohort.objects.filter(study=instance)
+        for cohort in cohorts:
+            # get participants in this cohort
+            participants = Participant.objects.filter(cohort=cohort)
+            for participant in participants:
+                if FeatureFlags.exists(user=participant.user):
+                    FeatureFlags.update(
+                        user=participant.user, flags=participant.cohort.study.studywide_feature_flags)
