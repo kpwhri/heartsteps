8e69dbbdd01222a57930073875d20e38d0005047
patrick9958 <patrick.neggie@gmail.com>
2021-08-10 19:22:45 -0400

    #261 Add/delete flags to Study working
    old flags that are deleted from Study.studywide_featureflags are deleted
    across all participants that are part of the Study.
    new flags added to Study.studywide_featureflags are added to every
    participant in the Study. unique participant level feature flags are
    preserved

--- server/participants/receivers.py
+++ server/participants/receivers.py
@@ -1,6 +1,6 @@
 import participants
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from feature_flags.models import FeatureFlags
 from participants.models import Study, Cohort, Participant
 
@@ -12,15 +12,51 @@ def create_feature_flags(sender, instance, created, **kwargs):
             user=instance.user, flags=instance.cohort.study.studywide_feature_flags)
 
 
-@receiver(post_save, sender=Study)
-def update_feature_flags(sender, instance, created, **kwargs):
-    if not created:
-        # get cohorts inside this study
-        cohorts = Cohort.objects.filter(study=instance)
-        for cohort in cohorts:
-            # get participants in this cohort
-            participants = Participant.objects.filter(cohort=cohort)
-            for participant in participants:
-                if FeatureFlags.exists(user=participant.user):
-                    FeatureFlags.update(
-                        user=participant.user, flags=participant.cohort.study.studywide_feature_flags)
+@receiver(pre_save, sender=Study)
+def update_feature_flags(sender, instance, **kwargs):
+    # get cohorts inside this study
+    cohorts = Cohort.objects.filter(study=instance)
+    for cohort in cohorts:
+        # get participants in this cohort
+        participants = Participant.objects.filter(cohort=cohort)
+        for participant in participants:
+            if FeatureFlags.exists(user=participant.user):
+                new_studywide_feature_flags_list = instance.studywide_feature_flags.split(
+                    ", ")
+                current_feature_flag_list = FeatureFlags.get(
+                    user=participant.user).flags.split(", ")
+                # will always return a unique Study object because name attribute is unique for each study
+                old_studywide_feature_flags_list = Study.objects.get(
+                    name=instance.name).studywide_feature_flags.split(", ")
+                # print("FF: new_studywide_feature_flags_list - ",
+                #       new_studywide_feature_flags_list)
+                # print("FF: old_studywide_feature_flags_list - ",
+                #       old_studywide_feature_flags_list)
+                # print("FF: current_feature_flag_list - ",
+                #       current_feature_flag_list)
+                # performs union operator on both feature flag lists without repetition
+
+                # i.e. old studywide list = ['hi', 'hello', 'wow']
+                # new studywide list = ['hi', 'wow', 'yikes']
+                # current stored feature flags list for user = ['hi', 'hello', 'wow', 'a_unique_flag']
+                # final flag to be used in update = ['hi', 'wow', 'yikes', 'a_unique_flag']
+                union_flags = list(set(new_studywide_feature_flags_list) | set(
+                    current_feature_flag_list))
+
+                # TODO: test more and make more efficient
+                outdated_flags = ['']
+                for old_flag in old_studywide_feature_flags_list:
+                    if old_flag not in new_studywide_feature_flags_list:
+                        outdated_flags.append(old_flag)
+
+                for old_flag in outdated_flags:
+                    if old_flag in union_flags:
+                        # print("FF: union removed - ", old_flag)
+                        union_flags.remove(old_flag)
+
+                final_flags = ", ".join(union_flags)
+                # print("FF: final flags list - ", union_flags)
+                # print("FF: final flags string - ", final_flags)
+
+                FeatureFlags.update(
+                    user=participant.user, flags=final_flags)
