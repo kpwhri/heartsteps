75a1bd309a3ac8d67e1b2ce80e563c16332ce628
Rahul D'Costa <rdcosta@umich.edu>
2021-08-18 13:21:50 -0400

    #273 Completed array representation in ts for client side user logs

--- client/src/pages/user-logs/user-logs.page.ts
+++ client/src/pages/user-logs/user-logs.page.ts
@@ -6,9 +6,9 @@ import { HeartstepsServer } from '@infrastructure/heartsteps-server.service';
 import { Router } from '@angular/router';
 
 class Log {
-    public date: Date;
-    public message: string;
-    public type: string;
+    public timestamp: string;
+    public status: string;
+    public action: string;
 }
 
 @Component({
@@ -16,26 +16,57 @@ class Log {
     selector: 'user-logs-page'
 })
 export class UserLogsPage {
-
-    public logs: Array<Log>;
-
+        public serializeduserlogs: Array;
     constructor(
         private heartstepsServer: HeartstepsServer,
         private router: Router
     ) {
-        this.update()
+        this.update();
     }
 
     private update() {
+        this.serializeduserlogs = []
         this.heartstepsServer.get('userlogs')
         .then((data) => {
             console.log(data);
-            console.log('GET LATEST GOAL: Got a response from the server');
+            console.log('GET LATEST USER LOGS: Got a response from the server');
+
+            var temparray = [];
+
+            for (let i = 0; i < data.logs.length; i++) {
+
+//                 console.log(data.logs[i].action);
+//                 console.log(data.logs[i].timestamp);
+//                 console.log(data.logs[i].status);
+//
+//                 serializeduserlogs[i].action = data.logs[i].action;
+//                 serializeduserlogs[i].timestamp = data.logs[i].timestamp;
+//                 serializeduserlogs[i].status = data.logs[i].status;
+
+                var temp = {timestamp: data.logs[i].timestamp, status: data.logs[i].status, action: data.logs[i].action};
+//                 serializeduserlogs.push(temp);
+                temparray.push(temp);
+            }
+            console.log(temparray);
+            this.serializeduserlogs = temparray.slice();
+            console.log(this.serializeduserlogs);
         })
         .catch(() => {
             console.log('User logs failed')
         })
     }
+
+//     private getArray() {
+//         var array = [1,2,3];  // As an example
+//
+//         var ans = "<TABLE><TR>";
+//         for(i=0; i<array.length; i++) {
+//             ans += "<TD>" + array[i] + "</TD>";
+//         }
+//         ans += "</TR></TABLE>"
+//
+//         document.write(ans);
+//     }
 
 //     private dateToDay(date: Date): Day {
 //         day.date = date;
