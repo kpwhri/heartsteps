aa1c133f721915ac02389f9259bd0928b3d8d81c
Junghwan Park <jup014@ucsd.edu>
2021-08-19 08:36:25 -0700

    update DayAdmin page - to add list_display

--- server/days/admin.py
+++ server/days/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Day
 
-admin.site.register(Day, admin.ModelAdmin)
+class DayAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date', 'timezone', 'start', 'end']
+    fields = ['user', 'date', 'timezone', 'start', 'end']
+
+admin.site.register(Day, DayAdmin)
