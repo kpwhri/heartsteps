29d031f3500646b42c796fcb0be3e030338d06ab
Junghwan Park <jup014@ucsd.edu>
2021-10-19 02:39:30 -0700

    convert fields to readonly_fields

--- server/bout_planning_notification/admin.py
+++ server/bout_planning_notification/admin.py
@@ -5,14 +5,14 @@ from .models import BoutPlanningNotification, Level, BoutPlanningDecision
 
 class FirstBoutPlanningTimeAdmin(admin.ModelAdmin):
     list_display = ['user', 'hour', 'minute', 'active']
-    fields = ['user', 'hour', 'minute', 'active']
+    readonly_fields = ['user', 'hour', 'minute', 'active']
 
 admin.site.register(FirstBoutPlanningTime, FirstBoutPlanningTimeAdmin)
 
 
 class BoutPlanningNotificationAdmin(admin.ModelAdmin):
     list_display = ['user', 'message', 'level', 'when']
-    fields = ['user', 'message', 'level', 'decision', 'when']
+    readonly_fields = ['user', 'message', 'level', 'decision', 'when']
 
 admin.site.register(BoutPlanningNotification, BoutPlanningNotificationAdmin)
 
@@ -20,13 +20,13 @@ admin.site.register(BoutPlanningNotification, BoutPlanningNotificationAdmin)
 
 class LevelAdmin(admin.ModelAdmin):
     list_display = ['user', 'level', 'date']
-    fields = ['user', 'level', 'date']
+    readonly_fields = ['user', 'level', 'date']
 
 admin.site.register(Level, LevelAdmin)
 
 
 class BoutPlanningDecisionAdmin(admin.ModelAdmin):
     list_display = ['user', 'N', 'O', 'R', 'return_bool', 'when_created']
-    fields = ['user', 'N', 'O', 'R', 'return_bool', 'when_created', 'data']
+    readonly_fields = ['user', 'N', 'O', 'R', 'return_bool', 'when_created', 'data']
     
 admin.site.register(BoutPlanningDecision, BoutPlanningDecisionAdmin)
