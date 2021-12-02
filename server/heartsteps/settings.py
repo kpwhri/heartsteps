2c40f9e558e6add6f1a831a74230ac86695359ff
Rahul D'Costa <rdcosta@umich.edu>
2021-12-02 00:57:06 -0500

    Enabling permissions to access step goal API

--- server/heartsteps/settings.py
+++ server/heartsteps/settings.py
@@ -180,7 +180,8 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
