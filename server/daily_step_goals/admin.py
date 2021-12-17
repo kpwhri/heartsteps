cfd6baee26cb3fbddfe071c8864cacd0a765930e
Junghwan Park <jup014@ucsd.edu>
2021-12-16 20:58:48 -0800

    changed to today, added admin for daily step goals

--- server/daily_step_goals/admin.py
+++ server/daily_step_goals/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import StepGoal
+from .models import StepGoal, StepGoalPRBScsv
 
 class StepGoalsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['user', 'date', 'step_goal']
+    readonly_fields = ['uuid', 'user', 'date', 'step_goal']
+    
 admin.site.register(StepGoal, StepGoalsAdmin)
+
+
+class StepGoalPRBScsvAdmin(admin.ModelAdmin):
+    list_display = ['cohort', 'PRBS_text', 'when_created', 'delimiter']
+    fields = ['cohort', 'PRBS_text', 'when_created', 'delimiter']
+    
+admin.site.register(StepGoalPRBScsv, StepGoalPRBScsvAdmin)
