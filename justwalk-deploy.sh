0013aaa27dc67f660833bf4e8b0fc84e2f2e6feb
Junghwan Park <jup014@ucsd.edu>
2022-01-12 10:34:21 -0800

    fixed justwalk GCP

--- justwalk-deploy.sh
+++ justwalk-deploy.sh
@@ -3,31 +3,29 @@
 gcloud auth activate-service-account --key-file=credentials/ucsd-publichealth-justwalk.json
 gcloud auth configure-docker -q
 gcloud config set project ucsd-publichealth-justwalk
-gcloud container clusters get-credentials cluster --region=us-west1-a
+gcloud container clusters get-credentials justwalk --region=us-west1-a
 
 cp credentials/.env-gcloud-dev server/.env
 cp credentials/justwalk.env server/justwalk.env
 cp credentials/.env-gcloud-dev client/.env
 
 docker-compose -f justwalk.docker-compose.yaml build nginx
-docker tag justwalk_nginx gcr.io/ucsd-publichealth-justwalk/justwalk-nginx
-docker push gcr.io/ucsd-publichealth-justwalk/justwalk-nginx
+docker compose -f justwalk.docker-compose.yaml push nginx
 
 docker-compose -f justwalk.docker-compose.yaml build server
-docker-compose run server python manage.py collectstatic --no-input
+docker-compose -f justwalk.docker-compose.yaml run server python manage.py collectstatic --no-input
 gsutil -m rsync -dr server/static gs://ucsd-publichealth-justwalk-media/static
-docker tag justwalk_server gcr.io/ucsd-publichealth-justwalk/justwalk-server
 docker push gcr.io/ucsd-publichealth-justwalk/justwalk-server
+
+docker-compose -f justwalk.docker-compose.yaml build client
 
 # This installs node_modules into the client directory,
 # which is missing because of docker-compose configuraiton
-docker-compose run client npm install
-
-docker-compose -f justwalk.docker-compose.yaml build client
-docker-compose run client npm run build:app --prod
+docker-compose -f justwalk.docker-compose.yaml run client npm install
+docker-compose -f justwalk.docker-compose.yaml run client npm run build:app --prod
 gsutil -m rsync -dr client/www gs://ucsd-publichealth-justwalk-media/app
 
-docker-compose run -e BUILD_NUMBER=$TRAVIS_BUILD_NUMBER client npm run build:website --prod
+docker-compose -f justwalk.docker-compose.yaml run -e BUILD_NUMBER=$TRAVIS_BUILD_NUMBER client npm run build:website --prod
 gsutil -m rsync -dr client/www gs://ucsd-publichealth-justwalk-media/website
 
 kubectl create secret generic credentials --from-file credentials
