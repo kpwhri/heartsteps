d05751529ab9bf4ac29048733a05cf80a31cbde2
Junghwan Park <jup014@ucsd.edu>
2022-02-11 09:39:10 -0800

    Comment out the error making part
    Comment out the error making part

--- server/feature_flags/receivers.py
+++ server/feature_flags/receivers.py
@@ -1,7 +1,10 @@
 
 # TODO: ASK WHAT THIS IMPORT IS INTENDED TO DO
 # from msilib.schema import Feature
-
+# -> msilib looks like a package for installing Windows software.
+# -> and my guess is AI such as copilot or something added it LOL
+# -> See this: https://github.com/kpwhri/heartsteps/commit/f8808beb4bb626ce0bae5fdacf6726c3c95143cf
+#   - Junghwan
 from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_save
 
@@ -19,39 +22,39 @@ def FeatureFlags_updated(instance, created, **kwargs):
         msg = "FeatureFlags updated: {}".format(instance.flags)
     EventLog.log(instance.user, msg, EventLog.INFO)
 
-@receiver(pre_save, sender=FeatureFlags)
-def adherence_message_check(instance, **kwargs):
-    participant = Participant.objects.get(user=instance.user)
-    print("HS DEBUG: participant: ", participant)
-    # config = AdherenceMessageConfiguration.objects.get(user=participant.user)
+# @receiver(pre_save, sender=FeatureFlags)
+# def adherence_message_check(instance, **kwargs):
+#     participant = Participant.objects.get(user=instance.user)
+#     print("HS DEBUG: participant: ", participant)
+#     # config = AdherenceMessageConfiguration.objects.get(user=participant.user)
 
-    def is_configuration_enabled():
-        if not participant.user:
-            return False
-        try:
-            configuration = AdherenceMessageConfiguration.objects.get(
-                user=participant.user)
-            return configuration.enabled
-        except AdherenceMessageConfiguration.DoesNotExist:
-            return False
+#     def is_configuration_enabled():
+#         if not participant.user:
+#             return False
+#         try:
+#             configuration = AdherenceMessageConfiguration.objects.get(
+#                 user=participant.user)
+#             return configuration.enabled
+#         except AdherenceMessageConfiguration.DoesNotExist:
+#             return False
 
-    if instance.has_flag("adherence_messages"):
-        # print("HS DEBUG: has flag adherence messages")
-        if not participant.user:
-            return
-        config = AdherenceMessageConfiguration.objects.get(
-            user=participant.user)
-        config.enabled = True
-        config.save()
-        # print("DEBUG: configuration is enabled")
-        # assert(is_configuration_enabled())
+#     if instance.has_flag("adherence_messages"):
+#         # print("HS DEBUG: has flag adherence messages")
+#         if not participant.user:
+#             return
+#         config = AdherenceMessageConfiguration.objects.get(
+#             user=participant.user)
+#         config.enabled = True
+#         config.save()
+#         # print("DEBUG: configuration is enabled")
+#         # assert(is_configuration_enabled())
     
-    # default behavior be to turn adherence messages off
-    else:
-        # print("HS DEBUG: does not have flag adherence messages")
-        config, _ = AdherenceMessageConfiguration.objects.get_or_create(
-            user=participant.user)
-        config.enabled = False
-        config.save()
-        # print("DEBUG: configuration is enabled")
-        # assert(not is_configuration_enabled())
+#     # default behavior be to turn adherence messages off
+#     else:
+#         # print("HS DEBUG: does not have flag adherence messages")
+#         config, _ = AdherenceMessageConfiguration.objects.get_or_create(
+#             user=participant.user)
+#         config.enabled = False
+#         config.save()
+#         # print("DEBUG: configuration is enabled")
+#         # assert(not is_configuration_enabled())
