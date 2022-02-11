0eb638f40065e382ec07801c3878ad43a93481bd
patrick9958 <patrick.neggie@gmail.com>
2022-02-10 20:49:25 -0500

    adherence message toggle now works

--- server/feature_flags/receivers.py
+++ server/feature_flags/receivers.py
@@ -3,7 +3,7 @@
 # from msilib.schema import Feature
 
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 from participants.models import Participant
 from adherence_messages.models import Configuration as AdherenceMessageConfiguration
@@ -19,9 +19,10 @@ def FeatureFlags_updated(instance, created, **kwargs):
         msg = "FeatureFlags updated: {}".format(instance.flags)
     EventLog.log(instance.user, msg, EventLog.INFO)
 
-@receiver(post_save, sender=FeatureFlags)
-def adherence_message_check(instance, created, **kwargs):
+@receiver(pre_save, sender=FeatureFlags)
+def adherence_message_check(instance, **kwargs):
     participant = Participant.objects.get(user=instance.user)
+    print("HS DEBUG: participant: ", participant)
     # config = AdherenceMessageConfiguration.objects.get(user=participant.user)
 
     def is_configuration_enabled():
@@ -33,19 +34,24 @@ def adherence_message_check(instance, created, **kwargs):
             return configuration.enabled
         except AdherenceMessageConfiguration.DoesNotExist:
             return False
-    
+
     if instance.has_flag("adherence_messages"):
+        # print("HS DEBUG: has flag adherence messages")
         if not participant.user:
             return
-        if is_configuration_enabled():
-            config = AdherenceMessageConfiguration.objects.get(
-                user=participant.user)
-            config.enabled = True
-            config.save()
+        config = AdherenceMessageConfiguration.objects.get(
+            user=participant.user)
+        config.enabled = True
+        config.save()
+        # print("DEBUG: configuration is enabled")
+        # assert(is_configuration_enabled())
     
     # default behavior be to turn adherence messages off
     else:
+        # print("HS DEBUG: does not have flag adherence messages")
         config, _ = AdherenceMessageConfiguration.objects.get_or_create(
             user=participant.user)
         config.enabled = False
         config.save()
+        # print("DEBUG: configuration is enabled")
+        # assert(not is_configuration_enabled())
