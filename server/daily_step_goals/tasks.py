ccb946ea1c0eb0a6efa80b043367bcf9162f19b7
Junghwan Park <jup014@ucsd.edu>
2022-11-30 12:52:07 -0800

    fixed update step goal task by using fitbit library's make_request() function

--- server/daily_step_goals/tasks.py
+++ server/daily_step_goals/tasks.py
@@ -85,7 +85,9 @@ def update_goal(username, day=None):
     assert isinstance(username, str), "username must be a string: {}".format(type(username))
     
     user = User.objects.get(username=username)
+    EventLog.info(user, "[update_goal] update_goal() launched")
     if user.is_active:
+        EventLog.info(user, "[update_goal] user.is_active")
         participant = Participant.objects.get(user=user)
         enrollment_date = participant.study_start_date
         day_service = DayService(user)
@@ -97,9 +99,11 @@ def update_goal(username, day=None):
         participant_service = participants.services.ParticipantService(user=user)
         if participant_service.is_baseline_complete():
             # The user is not in baseline period. moving on.
+            EventLog.info(user, "[update_goal] participant_service.is_baseline_complete()")
             pass
         else:
             # The user is in baseline period. Stopping here.
+            EventLog.info(user, "[update_goal] not participant_service.is_baseline_complete()")
             EventLog.info(user, "The user is in baseline period. Setting the goal to 10,000 steps per day, and stopping here.")
             BASELINE_STEPGOAL = 2000
             set_fixed_goal(user, day, BASELINE_STEPGOAL)
@@ -109,35 +113,45 @@ def update_goal(username, day=None):
         query = StepGoalsEvidence.objects.filter(user=user, startdate__lte=day, enddate__gte=day).order_by('-created')
         
         if not query.exists():
+            EventLog.info(user, "[update_goal] not query.exists()")
             EventLog.info(user, "No evidence found for today.")
             # no calculation evidence is found
             last_evidence_query = StepGoalsEvidence.objects.filter(user=user).order_by('-enddate', '-created')
             if not last_evidence_query.exists():
                 # no evidence is found at all
+                EventLog.info(user, "[update_goal] not last_evidence_query.exists()")
                 startdate = day
             else:
                 # some evidence is found
+                EventLog.info(user, "[update_goal] last_evidence_query.exists()")
                 last_evidence = last_evidence_query.first()
                 if last_evidence.enddate > day:
                     # calculation is completely wrong. re-calculating from the start
                     # startdate = enrollment_date + timedelta(days=baseline_period)
+                    EventLog.info(user, "[update_goal] last_evidence.enddate > day")
                     raise Exception("The last evidence is for the future. calculation is completely wrong")
                 else:
                     # calculation stopped sometime before
+                    EventLog.info(user, "[update_goal] not last_evidence.enddate > day")
                     startdate = last_evidence.enddate + timedelta(days=1)            
             
             while startdate <= day:
+                EventLog.info(user, "[update_goal] while startdate <= day: startdate={}, day={}".format(startdate, day))
                 evidence = stepgoal_service.calculate_step_goals(startdate=startdate)
                 startdate = evidence.enddate + timedelta(days=1)
         else:
             # this query is called repeatedly even there is an evidence that covers today.
             # recalculating and if it doesn't match with the previous records, we keep history.
+            EventLog.info(user, "[update_goal] query.exists()")
             evidence_for_today = query.first()
             startdate = evidence_for_today.startdate
             stepgoal_service.calculate_step_goals(startdate=startdate)
+        EventLog.info(user, "[update_goal] stepgoal_service.get_goal(day): day={}".format(day))
         step_goal = stepgoal_service.get_goal(day)
-        
+        EventLog.info(user, "[update_goal] update_fitbit_device_with_new_goal(user, step_goal): step_goal={}".format(step_goal))
         update_fitbit_device_with_new_goal(user, step_goal)
+    else:
+        EventLog.info(user, "[update_goal] not user.is_active")
     
 def set_fixed_goal(user, day, BASELINE_STEPGOAL):
     if StepGoal.objects.filter(user=user, date=day).exists():
@@ -151,15 +165,23 @@ def set_fixed_goal(user, day, BASELINE_STEPGOAL):
 
 # Temporary Goal Update API
 def update_fitbit_daily_stepgoal_temporary(user, steps):
-    fb_account = fitbit_api.models.FitbitAccountUser.objects.get(user=user).account
+    EventLog.info(user, "[update_goal] update_fitbit_daily_stepgoal_temporary() started.")
+    fa_service = fitbit_api.services.FitbitClient(user)
+    fb_account = fa_service.account
     fb_account_id = fb_account.fitbit_user
-    r = requests.post(
-        'https://api.fitbit.com/1/user/{}/activities/goals/daily.json?type=steps&value={}'.format(fb_account_id, steps),
-        headers={'Authorization': "Bearer {}".format(fb_account.access_token)}
-    )
-    return r.json()
+    EventLog.info(user, "[update_goal] fa_service.make_request() is prepared.")
+    r = fa_service.make_request('user/{}/activities/goals/daily.json?type=steps&value={}'.format(fa_service.account, steps), method='POST')
+    EventLog.info(user, "[update_goal] fa_service.make_request() is finished.")
+    EventLog.info(user, "[update_goal] r: {}".format(r))
+
+    if 'success' in r and not r['success']:
+        EventLog.error(user, "[update_goal] Fitbit request error: {}".format(r))
+        raise Exception(r)
+    return r
 
 def update_fitbit_device_with_new_goal(user, step_goal):
     # fitbit_service = fitbit_api.services.FitbitClient(user)    
     # fitbit_service.update_step_goals(step_goal)
+    EventLog.info(user, "[update_goal] update_fitbit_daily_stepgoal_temporary(user, step_goal) is being called: step_goal={}".format(step_goal))
     update_fitbit_daily_stepgoal_temporary(user, step_goal)
+    EventLog.info(user, "[update_goal] update_fitbit_daily_stepgoal_temporary(user, step_goal) is returned: step_goal={}".format(step_goal))
