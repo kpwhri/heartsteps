fca2f7288dcba884240e9307b454da01a7e0fc6d
Junghwan Park <jup014@ucsd.edu>
2023-03-24 16:51:55 -0700

    Add PowerPoint data visualization script and update .gitignore
    This commit adds a new script (db_dump/ppt.py) that generates a PowerPoint presentation containing data visualizations from the JustWalk database. The script uses heatmaps to visualize intervention components and user behavior measurements. The .gitignore file has been updated to ignore the output directory for the generated PowerPoint files.
    
    Additionally, the requirements.txt file in the db_dump folder has been updated to include the required packages for the new script.
    
    In this commit, we have implemented the following changes:
    - Added a new function, `draw_heatmap`, to create heatmap visualizations.
    - Implemented `JWPresentation` class to build PowerPoint presentations with data visualizations.
    - Modified the script to generate and display a heatmap for user levels based on the given date range.
    - Integrated Streamlit to provide an interactive interface for selecting date ranges and displaying the heatmap.
    - Merged `step_goal` data from the `daily_step_goals_stepgoal` collection into the `daily` DataFrame.
    - Stored the `daily` DataFrame into a new MongoDB collection named `daily`.

---
+++ db_dump/ppt.py
@@ -0,0 +1,119 @@
+from tools import get_mongodb_db, JWPresentation, get_uuid4_filename, draw_heatmap
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import pymongo
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+import datetime
+from pymongo import MongoClient
+
+
+# output directory
+output_dir = 'db_dump/output'
+
+# create output directory if it does not exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+
+filepath = os.path.join(output_dir, 'JustWalk data visualization ({}).pptx'.format(
+    pd.Timestamp.today().strftime('%Y-%m-%d')))
+
+# open a connection to the database
+tdb = get_mongodb_db()
+
+# fetch daily dataset
+daily = pd.DataFrame(list(tdb['daily'].find()))
+
+# Default Visualization Parameters
+cm_name = 'coolwarm'
+
+# 1. draw a heatmap of the levels
+level_df = get_intervention_daily_df(daily)
+figure_levels = draw_heatmap(level_df, 
+                            index='user_id', 
+                            columns='day_index', 
+                            values='level_int', 
+                            legend_labels=[
+                                'Random (RA)', 
+                                'N+R (NR)', 
+                                'N+O (NO)', 
+                                'Full (FU)'
+                                ], 
+                            xlabel='Day Index', 
+                            ylabel='User ID', 
+                            legend_title='Values', 
+                            figure_name='levels.png', 
+                            output_dir=output_dir)
+
+# 2. draw a heatmap of the goals
+goals_df = get_intervention_daily_df(daily)
+figure_goals = draw_heatmap(daily,
+                            index='user_id',
+                            columns='day_index',
+                            values='step_goal',
+                            legend_labels=None,
+                            xlabel='Day Index',
+                            ylabel='User ID',
+                            legend_title='Step Goals',
+                            figure_name='goals.png',
+                            output_dir=output_dir)
+
+
+jwp = JWPresentation()
+
+# add a table of contents
+jwp.toc = {
+    'type': 'section',
+    'title': 'Table of Contents',
+    'items': [
+        {
+            'type': 'section',
+            'title': 'Intervention Components',
+            'items': [
+                {
+                    'type': 'slide',
+                    'title': 'Levels',
+                    'figure': figure_levels
+                },
+                {
+                    'type': 'slide',
+                    'title': 'Goals',
+                    'figure': figure_goals
+                },
+                {
+                    'type': 'slide',
+                    'title': 'Notifications',
+                    'items': []
+                }
+            ]
+        },
+        {
+            'type': 'section',
+            'title': 'Behavioral Measurements',
+            'items': [
+                {
+                    'type': 'slide',
+                    'title': 'Steps',
+                    'items': []
+                },
+                {
+                    'type': 'slide',
+                    'title': 'Heart Rates',
+                    'items': []
+                },
+                {
+                    'type': 'slide',
+                    'title': 'Activities',
+                    'items': []
+                }
+            ]
+        }
+    ]
+}
+
+jwp.build()
+jwp.save(filepath)
+jwp.open()
--- db_dump/transform.py
+++ db_dump/transform.py
@@ -1,6 +1,5 @@
 from tools import get_mongodb_db, drop_all_collections, build_df_from_collection, extend_df_with_collection
 
-import streamlit as st
 import pandas as pd
 import numpy as np
 import os
@@ -51,7 +50,7 @@ df = extend_df_with_collection(df, db, 'participants_participant', {'cohort_id':
                                  '_id': 0, 'heartsteps_id': 1, 'cohort_id': 1, 'user_id': 1, 'birth_year': 1, 'study_start_date': 1}, on='cohort_id')
 
 df = df[['heartsteps_id', 'user_id', 'birth_year', 'study_start_date']]
-st.write("Participants are loaded: {}".format(df.shape[0]))
+print("Participants are loaded: {}".format(df.shape[0]))
 
 participants_collection = tdb['participants']
 participants_collection.insert_many(df.to_dict('records'))
@@ -79,65 +78,73 @@ pipeline = [
         '$project': {
             '_id': 0,
             'user_id': '$_id.user_id',
-            'date': '$_id.date',
-            'level': '$last_level',
+            'date_str': '$_id.date',
+            'level_str': '$last_level',
             'when_created': '$when_created'
         }
     }
 ]
 
 
-df = pd.DataFrame(db['bout_planning_notification_level'].aggregate(pipeline))
-df = df[['user_id', 'date', 'level']]
+daily = pd.DataFrame(db['bout_planning_notification_level'].aggregate(pipeline))
+daily = daily[['user_id', 'date_str', 'level_str']]
 
-# add a column of day_index per user_id, day_index is the number of days since the study start date
-df['day_index'] = df.groupby('user_id')['date'].rank(method='first')
+# add a column of day_index per user_id, day_index is the number of days since the study start date from participant collection
+participant_df = pd.DataFrame(participants_collection.find({}, {'_id': 0, 'user_id': 1, 'study_start_date': 1}))
+daily = pd.merge(daily, participant_df, on='user_id', how='left')
+daily['study_start_date'] = pd.to_datetime(daily['study_start_date'])
+daily['date_dt'] = pd.to_datetime(daily['date_str'])
+daily['day_index'] = (daily['date_dt'] - daily['study_start_date']).dt.days
 
-st.write("Intervention components are loaded: {}".format(df.shape[0]))
-st.write(df.sort_values(by=['user_id', 'date'], ascending=True))
-
-intervention_levels_collection = tdb['intervention_levels']
-intervention_levels_collection.insert_many(df.to_dict('records'))
-
-
-# 2.1.1 draw a heatmap of the levels, x axis is the date, y axis is the user_id (ordered), color is the level, add the legend, add the title, add a slider to select the date range
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-import datetime
-
-# Convert date to datetime format
-df['date'] = pd.to_datetime(df['date'])
 level_categories = ["RE", "RA", "NR", "NO", "FU"]
-df['level'] = pd.Categorical(df['level'], categories=level_categories, ordered=True).codes
+daily['level_int'] = pd.Categorical(daily['level_str'], categories=level_categories, ordered=True).codes
 
-# Pivot dataframe to create heatmap data
-heatmap_data = df.pivot_table(index='user_id', columns='date', values='level', aggfunc=np.sum)
+daily = daily[['user_id', 'date_str', 'date_dt', 'day_index', 'level_str', 'level_int']]
 
-# Create a Streamlit slider to select date range
-start_date = st.date_input('Start date', datetime.date(2022, 1, 1))
-end_date = st.date_input('End date', datetime.date(2023, 12, 31))
+# leave only the records with day_index >= 10 and day_index <= 252
+daily = daily[(daily['day_index'] >= 10) & (daily['day_index'] <= 252)]
 
-# Convert date objects to datetime objects
-start_date = pd.to_datetime(start_date)
-end_date = pd.to_datetime(end_date)
+print("Intervention components are loaded: {}".format(daily.shape[0]))
+print(daily.sort_values(by=['user_id', 'date_str'], ascending=True))
 
-# Filter heatmap data based on the selected date range
-mask = (heatmap_data.columns >= start_date) & (heatmap_data.columns <= end_date)
-filtered_heatmap_data = heatmap_data.loc[:, mask]
+# 2.2. find the `step_goal` from daily_step_goals_stepgoal, for each user_id and date, there are multiple goals, we only need the last goal by created column
 
-# Create heatmap
-plt.figure(figsize=(15, 10))
-sns.heatmap(filtered_heatmap_data, cmap='viridis', cbar_kws={'ticks': [0, 1, 2, 3, 4]})
+pipeline = [
+    {
+        '$match': {'user_id': {'$in': participant_list}}
+    },
+    {
+        '$sort': {'created': 1}
+    },
+    {
+        '$group': {
+            '_id': {'user_id': '$user_id', 'date': '$date'},
+            'last_goal': {'$last': '$step_goal'},
+            'created': {'$last': '$created'}
+        }
+    },
+    {
+        '$project': {
+            '_id': 0,
+            'user_id': '$_id.user_id',
+            'date_str': '$_id.date',
+            'step_goal': '$last_goal'
+        }
+    }
+]
 
-# Add title, legend and labels
-plt.title('User Levels Heatmap')
-plt.xlabel('Date')
-plt.ylabel('User ID')
+temp_goals_df = pd.DataFrame(db['daily_step_goals_stepgoal'].aggregate(pipeline))
 
-# Add legend
-plt.legend(title='Level', labels=level_categories)
+# merge the step_goal with daily dataframe
+daily = pd.merge(daily, temp_goals_df, on=['user_id', 'date_str'], how='right')
 
-# Display heatmap in Streamlit
-st.pyplot(plt.gcf())
+print("Goals are loaded: {}".format(daily.shape[0]))
+print(daily.sort_values(by=['user_id', 'date_str'], ascending=True))
 
+
+daily_collection = tdb['daily']
+
+# clean up the data before inserting into the database
+daily['date_dt'] = pd.to_datetime(daily['date_str'])
+
+daily_collection.insert_many(daily.to_dict('records'))
