8e68b19ba5da685695d1b98682ac9b17bea956c6
Ben Marlin <marlin@cs.umass.edu>
2023-04-20 16:00:13 -0400

    add debug script

---
+++ data-export/debug.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import pytz
+import numpy as np
+from django.utils import timezone
+import os
+from datetime import datetime,  date, timedelta, timezone
+from math import floor
+
+import utils
+
+from days.models import Day
+from days.services import DayService
+from contact.models import ContactInformation
+from fitbit_activities.models import FitbitDay
+from fitbit_activities.models import FitbitMinuteStepCount
+from fitbit_activities.models import FitbitMinuteHeartRate
+from fitbit_activities.models import FitbitActivity
+
+from fitbit_api.models import FitbitAccount
+from fitbit_api.models import FitbitAccountUser
+from locations.models import Location
+from locations.models import Place
+
+from push_messages.models import Message as PushMessage
+from walking_suggestions.models import Configuration as WalkingSuggestionConfiguration
+from weekly_reflection.models import ReflectionTime
+
+from participants.services import ParticipantService
+from participants.models import Cohort
+from participants.models import DataExport
+from participants.models import DataExportSummary
+from participants.models import DataExportQueue
+from participants.models import Study
+from participants.models import Participant
+
+from weeks.models import Week
+from surveys.models import Survey
+from page_views.models import PageView
+from activity_plans.models import  ActivityPlan
+from activity_logs.models import ActivityLog
+
+from morning_messages.models import MorningMessage
+
+    
+uid = user["uid"]
+username = user["hsid"]
+    
+import code
+code.interact(local=dict(globals(), **locals()))
+
