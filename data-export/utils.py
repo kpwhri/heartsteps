5985fac8a6c8a9ffe385a495574a595b2e7cea20
Ben Marlin <marlin@cs.umass.edu>
2023-04-19 22:11:54 -0400

    add survey open time to daily

--- data-export/utils.py
+++ data-export/utils.py
@@ -161,6 +161,9 @@ def estimate_survey_dwell_times(user,survey_type="weekly"):
     if(survey_type not in ["morning", "weekly"]):
         raise ValueError("Survey type must be morning or weekly")
 
+    import code
+    code.interact(local=dict(globals(), **locals()))
+
     days = Day.objects.filter(user_id=user).order_by("date").all()
     allPageViews=PageView.objects.filter(user_id=user).order_by("created").all()
     survey_pages = [x for x in allPageViews  if f"/{survey_type}-survey/survey"== x.uri or "/dashboard" in x.uri]
