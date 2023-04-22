18d3d1ac78c48ef5b6821bb7cd8b677c9a417c51
Ben Marlin <marlin@cs.umass.edu>
2023-04-21 23:48:36 -0400

    updating antisedintary suggestions

--- data-export/within_day.py
+++ data-export/within_day.py
@@ -5,44 +5,12 @@ from django.utils import timezone
 import os
 from datetime import datetime,  date, timedelta, timezone
 from math import floor
-
+from collections import defaultdict
 import utils
 
 from days.models import Day
-from days.services import DayService
-from contact.models import ContactInformation
-from fitbit_activities.models import FitbitDay
-from fitbit_activities.models import FitbitMinuteStepCount
-from fitbit_activities.models import FitbitMinuteHeartRate
-from fitbit_activities.models import FitbitActivity
-
-from fitbit_api.models import FitbitAccount
-from fitbit_api.models import FitbitAccountUser
-from locations.models import Location
-from locations.models import Place
-
-from push_messages.models import Message as PushMessage
-from walking_suggestions.models import Configuration as WalkingSuggestionConfiguration
-from weekly_reflection.models import ReflectionTime
-
-from participants.services import ParticipantService
-from participants.models import Cohort
-from participants.models import DataExport
-from participants.models import DataExportSummary
-from participants.models import DataExportQueue
-from participants.models import Study
-from participants.models import Participant
-
-from weeks.models import Week
-from surveys.models import Survey
-from page_views.models import PageView
-from activity_plans.models import  ActivityPlan
-from activity_logs.models import ActivityLog
-
-from morning_messages.models import MorningMessage
-
 from walking_suggestions.models import WalkingSuggestionDecision
-from collections import defaultdict
+from anti_sedentary.models import AntiSedentaryDecision
 
 def walking_suggestions(user,directory = None, filename = None, start=None, end=None, from_scratch=True,DEBUG=True):
     
