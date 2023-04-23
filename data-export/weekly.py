ae3b3d4af6b408bc317a9f12b8a0608d70304cf0
Ben Marlin <marlin@cs.umass.edu>
2023-04-23 09:10:07 -0400

    update weekly survey

--- data-export/weekly.py
+++ data-export/weekly.py
@@ -205,6 +205,7 @@ def export_weekly_survey(user,directory = None, filename = None, start=None, end
                 'answered'}
     for f,n in base_fields.items():
         df[n]=df["Object"].map(lambda x: getattr(x,f))
+    df=df.set_index("Study Week")
 
     #Re-map barriers list to string with : separator
     #There is not a fixed list of barriers
@@ -228,7 +229,7 @@ def export_weekly_survey(user,directory = None, filename = None, start=None, end
     df_msg['Notification Time Received'] = df_msg['Object'].map(lambda msg: localize_time(msg._message_receipts["received"], tz_lookup) if "received" in msg._message_receipts else pd.NaT)
     df_msg['Notification Time Opened']   = df_msg['Object'].map(lambda msg: localize_time(msg._message_receipts["opened"], tz_lookup) if "opened" in msg._message_receipts else pd.NaT)
     df_msg=df_msg.drop(labels="Object", axis=1)
-    #df_msg = df_msg.set_index("Study Week")
+    df_msg = df_msg.set_index("Study Week")
 
     #Join weekly survey with notifications
     df = df.join(df_msg, on="Study Week",how="outer")
