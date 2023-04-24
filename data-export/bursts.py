1047cd9f8b59dda51d7929a562a728e70e3fa1b1
Ben Marlin <marlin@cs.umass.edu>
2023-04-24 09:34:43 -0400

    update burst walking survey

--- data-export/bursts.py
+++ data-export/bursts.py
@@ -102,10 +102,11 @@ def export_burst_walking_survey2(user,directory = None, filename = None, start=N
 
     df.to_csv(os.path.join(directory,filename))
 
-def export_burst_walking_survey(user,directory = None, filename = None, start=None, end=None, from_scratch=True, DEBUG=True):
+
+def export_burst_survey(user,queryset,survey_type,questions,directory = None, filename = None, start=None, end=None, from_scratch=True, DEBUG=True):
 
     """
-    Construct dataframe from ActivitySurvey data model and export to csv
+    Construct dataframe from a burst survey data model and export to csv
     """
 
     uid = user["uid"]
@@ -130,6 +131,104 @@ def export_burst_walking_survey(user,directory = None, filename = None, start=No
     ndt=utils.estimate_notification_dwell_times(uid)
 
     # Get walking suggestion survey objects
+    df = pd.DataFrame({'Object': [x for x in queryset]})
+
+    df["Datetime"] = df['Object'].map(lambda x: localize_time(x.created, tz_lookup))
+    
+    df["Survey Probability"] = df['Object'].map(lambda x: x.decision.treatment_probability if hasattr(x,"decision") else 1)
+
+    #Lookup notifications and map receipts
+    notification_lookup = utils.get_survey_notifications(uid, survey_type)
+    df["receipts"] = df['Object'].map(lambda x: notification_lookup[x.uuid]._message_receipts)
+
+    df['Notification Was Sent']      = df["receipts"].map(lambda x: "sent" in x)
+    df['Notification Was Received']  = df["receipts"].map(lambda x: "received" in x)
+    df['Notification Was Opened']    = df["receipts"].map(lambda x: "opened" in x)
+    df['Notification Time Sent']     = df["receipts"].map(lambda x: localize_time(x["sent"], tz_lookup) if "sent" in x else pd.NaT)
+    df['Notification Time Received'] = df["receipts"].map(lambda x: localize_time(x["received"], tz_lookup) if "received" in x else pd.NaT)
+    df['Notification Time Opened']   = df["receipts"].map(lambda x: localize_time(x["opened"], tz_lookup) if "opened" in x else pd.NaT)
+
+    #Survey time details
+    sot = df['Object'].map(lambda x: get_survey_open_time(str(notification_lookup[x.uuid].uuid),tz_lookup,ndt)) #opend time
+    sat = df['Object'].map(lambda x: localize_time(x.updated,tz_lookup) if x.answered else pd.NaT) #answered time
+    df["Survey Was Opened"]   = sot.map(lambda x: x is not pd.NaT) 
+    df["Survey Was Answered"] = df["Object"].map(lambda x: x.answered)
+    df["Survey Opened Time"]  =sot
+    df["Survey Answered Time"]=sat
+    df['Survey Time Spent Answering'] = (sat-sot).map(lambda x: np.round(x.total_seconds(),1) if (x is not None and x is not np.nan and not pd.isnull(x)) else x)
+
+    #Get survey answers dictionary and map
+    df["answers"]=df["Object"].map(lambda x: x.get_answers())
+    for f,n in questions.items():
+        df[n]=df["answers"].map(lambda x: x[f] if f in x else None)
+
+    #Set index and drop extra columns
+    df["Particiant ID"]=username
+
+    #Map time fields to strings
+    time_fields = ['Datetime','Notification Time Sent',
+                   'Notification Time Received',
+                   'Notification Time Opened',
+                   "Survey Opened Time",
+                   "Survey Answered Time"]
+    for f in time_fields:
+        df[f] = df[f].map(to_time_str)
+
+    df = df.set_index(["Particiant ID", "Datetime"]) 
+    df = df.drop(labels=["answers","Object","receipts"],axis=1)
+
+    if(DEBUG):
+        import code
+        code.interact(local=dict(globals(), **locals()))
+
+    return df
+
+
+def export_burst_walking_survey(user,directory = None, filename = None, start=None, end=None, from_scratch=True, DEBUG=True):
+
+    """
+    Construct dataframe from ActivitySurvey data model and export to csv
+    """
+
+    uid = user["uid"]
+    username = user["hsid"]
+
+    # Export Destination
+    if not directory: 
+        directory = './'
+    if not filename:
+        filename = '{username}.burst_walking_survey.csv'.format(
+            username=username
+        )
+
+    # Skip rewriting if exists and trusted (no new data)
+    if not from_scratch and os.path.isfile(os.path.join(directory, filename)):
+        return
+
+    queryset = WalkingSuggestionSurvey.objects.filter(user_id=uid).order_by('created', 'updated').all()
+
+    questions={'enjoyment':"Enjoyment of Activities",
+        'fit':"Fit with Routine", 
+        'social_support':"Social Support", 
+        'intrinsic_motivation':"Intrinsic Motivation", 
+        'extrinsic_motivation':"Extrinsic Motivation"
+    }
+
+    survey_type = 'Walking Suggestion Survey'
+
+    df = export_burst_survey(user,queryset,survey_type,questions,directory = None, filename = None, start=None, end=None, from_scratch=True, DEBUG=True):
+
+    df.to_csv(os.path.join(directory, filename))
+
+    return
+
+    #Get survey indicators
+    #Get days, timezones, and survey dweel time
+    days      = Day.objects.filter(user_id=uid).order_by("date").all()
+    tz_lookup = {x.date: pytz.timezone(x.timezone) for x in days}
+    ndt=utils.estimate_notification_dwell_times(uid)
+
+    # Get walking suggestion survey objects
     query = WalkingSuggestionSurvey.objects.filter(user_id=uid).order_by('created', 'updated').all()
     df = pd.DataFrame({'Object': [x for x in query]})
 
@@ -146,8 +245,6 @@ def export_burst_walking_survey(user,directory = None, filename = None, start=No
     df['Notification Time Sent']     = df["receipts"].map(lambda x: localize_time(x["sent"], tz_lookup) if "sent" in x else pd.NaT)
     df['Notification Time Received'] = df["receipts"].map(lambda x: localize_time(x["received"], tz_lookup) if "received" in x else pd.NaT)
     df['Notification Time Opened']   = df["receipts"].map(lambda x: localize_time(x["opened"], tz_lookup) if "opened" in x else pd.NaT)
-
-    #df["notification_id"] = df['Object'].map(lambda x: notification_lookup[x.uuid].uuid)
 
     #Survey time details
     asot = df['Object'].map(lambda x: get_survey_open_time(str(notification_lookup[x.uuid].uuid),tz_lookup,ndt))
