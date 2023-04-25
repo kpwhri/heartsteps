99cd43236eb18f88b24b812166f364b776d2db46
Ben Marlin <marlin@cs.umass.edu>
2023-04-24 21:25:52 -0400

    update daily planning

--- data-export/daily.py
+++ data-export/daily.py
@@ -57,6 +57,7 @@ def export_daily_planning_data(user,directory = None, filename = None, start=Non
                         "Duration Marked Completed":"Total Duration of Activities Planned on This Day Marked Completed"
                 }
     df1 = df1.rename(columns=column_map)
+    df1 = df1[list(column_map.values())[1:]]
 
     #Group activities by date they were planned to be performed on
     df2 = df[["Activity Date","Duration","Duration Marked Completed","Vigorous","Number","Marked Completed"]].groupby("Activity Date").sum()
