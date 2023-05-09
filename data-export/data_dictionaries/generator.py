4e0bf23fdc4345391ba4ce7d4e5fffe9a9a34e58
Benjamin Fenelon <bfenelon@1x-nat-vl931-172-30-153-110.wireless.umass.edu>
2023-05-08 21:32:35 -0400

    Data Dictionary generator

---
+++ data-export/data_dictionaries/generator.py
@@ -0,0 +1,51 @@
+import run
+import utils
+import argparse
+import os
+import glob
+import pandas as pd
+
+DICTIONARY_DIR = "/"
+
+
+def generate_dictionaries(export_loc: str, hsid: str):
+
+    # extract csvs from all runs of user
+    exports = glob.glob(os.path.join(export_loc, hsid, "*.csv"))
+
+    # parse heading
+    for export in exports:
+        dataframe = pd.read_csv(export)
+
+        # Visualize Ranges
+        print(dataframe.describe())
+
+        # Create Dictionary
+        dictionary = pd.DataFrame({'ElementName': dataframe.columns, 'DataType': dataframe.dtypes})
+
+        # Write Dictionary
+        # Standardizes logs. exports
+        export_name = "_".join(os.path.basename(export).split('.')[1:-1])
+
+        print(f'Writing {export_name} dictionary')
+        dictionary.to_csv(os.path.join(DICTIONARY_DIR, f'{export_name}.csv'))
+
+
+if __name__ == "__main__":
+    # arg collect
+    parser = argparse.ArgumentParser(description='Generate Data Dictionaries')
+    parser.add_argument('-r', '--refresh', help='run new export before generating', dest='export', action='store_true')
+    parser.add_argument('hsid', help='user to use as ground truth')
+    args = parser.parse_args()
+
+    # run clean export
+    config = utils.read_config()
+    if args.export:
+        print("Running new -all export")
+        run.export_all_data(config["EXPORT_DIR"], cohort='U01', exports=["all"], DEBUG=False)
+
+    EXPORT_LOCATION = os.path.join(config["EXPORT_DIR"], 'U01')
+
+    print(f"Exporting Data Dictionaries from user {args.hsid}")
+
+    generate_dictionaries(EXPORT_LOCATION, hsid=args.hsid)
